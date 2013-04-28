58cee7a5534fd983a6f73aa9c119cf48da7c9d5a
Kristof Adriaenssens <kristof.adriaenssens@gmail.com>
2013-04-28 16:18:40 +0200

    Improve javadoc of public api.

--- brave-impl/src/main/java/com/github/kristofa/brave/Brave.java
+++ brave-impl/src/main/java/com/github/kristofa/brave/Brave.java
@@ -19,8 +19,13 @@ public class Brave {
     }
 
     /**
-     * Gets {@link EndPointSubmitter}. Allows you to set endpoint (ip, port, service name) for this service. Should be set
-     * only once.
+     * Gets {@link EndPointSubmitter}. Allows you to set endpoint (ip, port, service name) for this service.
+     * <p/>
+     * Each annotation that is being submitted (including cs, cr, sr, ss) has an endpoint (host, port, service name)
+     * assigned. For a given service/application instance the endpoint only needs to be set once and will be reused for all
+     * submitted annotations.
+     * <p/>
+     * The EndPoint needs to be set using the EndPointSubmitter before any annotation/span is created.
      * 
      * @return {@link EndPointSubmitter}.
      */
@@ -30,6 +35,8 @@ public class Brave {
 
     /**
      * Gets a simple {@link SpanCollector} which logs spans through slf4j at info level.
+     * <p/>
+     * Can be used for testing or debugging.
      * 
      * @return A simple {@link SpanCollector} which logs spans through slf4j at info level.
      * @see Brave#getClientTracer(SpanCollector)
@@ -40,7 +47,9 @@ public class Brave {
     }
 
     /**
-     * Gets a {@link TraceFilter} that does not filtering at all.
+     * Gets a {@link TraceFilter} that does not filter. So it will trace every request.
+     * <p/>
+     * Can be used if tracing every request will not cause too much overhead or for debugging or during development.
      * 
      * @return TraceFilter that does not filtering at all.
      * @see Brave#getClientTracer(SpanCollector, TraceFilter)
@@ -52,6 +61,10 @@ public class Brave {
     /**
      * Gets a {@link ClientTracer} that will be initialized with a custom {@link SpanCollector} and a custom
      * {@link TraceFilter}.
+     * <p/>
+     * The ClientTracer is used to initiate a new span when doing a request to another service. It will generate the cs
+     * (client send) and cr (client received) annotations. When the cr annotation is set the span will be submitted to
+     * SpanCollector if not filtered by TraceFilter.
      * 
      * @param collector Custom {@link SpanCollector}. Should not be <code>null</code>.
      * @param traceFilter Custom trace filter. Should not be <code>null</code>.
@@ -65,6 +78,9 @@ public class Brave {
 
     /**
      * Gets a {@link ServerTracer}.
+     * <p/>
+     * The ServerTracer is used to generate sr (server received) and ss (server send) annotations. When ss annotation is set
+     * the span will be submitted to SpanCollector if our span needs to get traced (as decided by ClientTracer).
      * 
      * @param collector Custom {@link SpanCollector}. Should not be <code>null</code>.
      * @return {@link ServerTracer} instance.
