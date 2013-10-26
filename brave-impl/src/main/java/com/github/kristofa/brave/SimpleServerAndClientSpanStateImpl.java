98a76088e5378974ec925660db0922c44888d568
Kristof Adriaenssens <kristof.adriaenssens@gmail.com>
2013-10-26 10:11:04 +0200

    Add javadoc and change formatting.

--- brave-impl/src/main/java/com/github/kristofa/brave/SimpleServerAndClientSpanStateImpl.java
+++ brave-impl/src/main/java/com/github/kristofa/brave/SimpleServerAndClientSpanStateImpl.java
@@ -3,79 +3,87 @@ package com.github.kristofa.brave;
 import com.twitter.zipkin.gen.Endpoint;
 import com.twitter.zipkin.gen.Span;
 
+/**
+ * {@link ServerAndClientSpanState} implementation that is used with {@link BraveContext}.
+ * 
+ * @see BraveContext
+ */
 class SimpleServerAndClientSpanStateImpl implements ServerAndClientSpanState {
-	
-	private Endpoint endPoint;
-	private Span currentClientSpan;
-	private ServerSpan currentServerSpan;
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public ServerSpan getCurrentServerSpan() {
-		return currentServerSpan;
-	}
+    private Endpoint endPoint;
+    private Span currentClientSpan;
+    private ServerSpan currentServerSpan;
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void setCurrentServerSpan(final ServerSpan span) {
-		currentServerSpan = span;
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public ServerSpan getCurrentServerSpan() {
+        return currentServerSpan;
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public Endpoint getEndPoint() {
-		return endPoint;
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void setCurrentServerSpan(final ServerSpan span) {
+        currentServerSpan = span;
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void setEndPoint(final Endpoint endPoint) {
-		this.endPoint = endPoint;
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Endpoint getEndPoint() {
+        return endPoint;
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public Span getCurrentClientSpan() {
-		return currentClientSpan;
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void setEndPoint(final Endpoint endPoint) {
+        this.endPoint = endPoint;
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void setCurrentClientSpan(final Span span) {
-		currentClientSpan = span;
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Span getCurrentClientSpan() {
+        return currentClientSpan;
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void incrementServerSpanThreadDuration(final long durationMs) {
-		currentServerSpan.incThreadDuration(durationMs);
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void setCurrentClientSpan(final Span span) {
+        currentClientSpan = span;
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public long getServerSpanThreadDuration() {
-		return currentServerSpan.getThreadDuration();
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void incrementServerSpanThreadDuration(final long durationMs) {
+        currentServerSpan.incThreadDuration(durationMs);
+    }
 
-	@Override
-	public Boolean sample() {
-		return currentServerSpan.getSample();
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public long getServerSpanThreadDuration() {
+        return currentServerSpan.getThreadDuration();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Boolean sample() {
+        return currentServerSpan.getSample();
+    }
 
 }
