900734636b53abeb64cbaf6015d39c20d654f22c
Lei Gu <leigu@yahoo.com>
2014-01-31 14:31:35 -0500

    Add enable/disable flag and change submitEndpoint based on code review feedback

--- brave-impl/src/main/java/com/github/kristofa/brave/BraveTracer.java
+++ brave-impl/src/main/java/com/github/kristofa/brave/BraveTracer.java
@@ -10,11 +10,13 @@ import com.github.kristofa.brave.ServerTracer;
 public class BraveTracer {
     private static final String REQUEST_ANNOTATION = "request";
     private static final String FAILURE_ANNOTATION = "failure";
-    private final static Logger LOGGER = LoggerFactory.getLogger(BraveTracerTest.class);
+    
+    private final static Logger LOGGER = LoggerFactory.getLogger(BraveTracer.class);
 
 	ClientTracer clientTracer;
 	ServerTracer serverTracer;
 	EndPointSubmitter endPointSubmitter;
+	boolean enabled = true;
 	
 	public BraveTracer(ClientTracer clientTracer,
 			ServerTracer serverTracer, EndPointSubmitter endPointSubmitter) {
@@ -23,48 +25,86 @@ public class BraveTracer {
 		this.serverTracer = serverTracer;
 		this.endPointSubmitter = endPointSubmitter;
 	}	
+	
+	public BraveTracer(ClientTracer clientTracer,
+			ServerTracer serverTracer, EndPointSubmitter endPointSubmitter, boolean enabled) {
+		this(clientTracer, serverTracer, endPointSubmitter);
+		this.enabled = enabled;
+	}
+	
 	public void submitFailure()
 	{
-        clientTracer.submitAnnotation(FAILURE_ANNOTATION);	
+		if (enabled)
+		{
+			clientTracer.submitAnnotation(FAILURE_ANNOTATION);	
+		}
 	}
+	
 	public void submitBinaryAnnotation(String name, int value)
 	{
-		clientTracer.submitBinaryAnnotation(name, value);
+		if (enabled)
+		{
+			clientTracer.submitBinaryAnnotation(name, value);
+		}
 	}
 	public void submitBinaryAnnotation(String name, String value)
 	{
-		clientTracer.submitBinaryAnnotation(name, value);		
+		if (enabled)
+		{
+			clientTracer.submitBinaryAnnotation(name, value);	
+		}
 	}
 	public void submitAnnotation(String name, String value)
 	{
-		clientTracer.submitAnnotation(value);
+		if (enabled)
+		{
+			clientTracer.submitAnnotation(value);
+		}
 	}
 	public void stopServerTracer()
 	{
-		serverTracer.setServerSend();
+		if (enabled)
+		{
+			serverTracer.setServerSend();
+		}
 	}
 	public void startClientTracer(String clientContext)
 	{
-		clientTracer.startNewSpan(clientContext);
-        clientTracer.submitBinaryAnnotation(REQUEST_ANNOTATION, clientContext);
-        clientTracer.setClientSent();
+		if (enabled)
+		{
+			clientTracer.startNewSpan(clientContext);
+	        clientTracer.submitBinaryAnnotation(REQUEST_ANNOTATION, clientContext);
+	        clientTracer.setClientSent();
+		}
 	}
 	public void stopClientTracer()
 	{
-		clientTracer.setClientReceived();
+		if (enabled)
+		{
+			clientTracer.setClientReceived();
+		}
 	}
 	public void startServerTracer(String contextPath)
 	{
-		submitEndpoint(contextPath);
-        LOGGER.debug("Received no span state.");
-        serverTracer.setStateUnknown(contextPath);
-        serverTracer.setServerReceived();
+		if (enabled)
+		{
+			submitEndpoint(contextPath);
+	        LOGGER.debug("Received no span state.");
+	        serverTracer.setStateUnknown(contextPath);
+	        serverTracer.setServerReceived();
+		}
 	}
 	public void submitEndpoint(String contextPath) {
-        final String localAddr = "localhost";
-        final int localPort =0;
-        LOGGER.debug("Setting endpoint: addr: {}, port: {}, contextpath: {}", localAddr, localPort, contextPath);
-        endPointSubmitter.submit(localAddr, localPort, contextPath);
+		if (enabled)
+		{
+	        final String localAddr = "localhost";
+	        final int localPort =0;
+	        LOGGER.debug("Setting endpoint: addr: {}, port: {}, contextpath: {}", localAddr, localPort, contextPath);
+	        if (!endPointSubmitter.endPointSubmitted())
+	        {
+	        	endPointSubmitter.submit(localAddr, localPort, contextPath);
+	        }
+		}
     }
 
 }
