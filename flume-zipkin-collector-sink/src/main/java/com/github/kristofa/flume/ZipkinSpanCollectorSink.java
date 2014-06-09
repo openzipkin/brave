c12c409b52bc8ef016aa245994d80e2b36c2a2eb
Kristof A <kristof.adriaenssens@gmail.com>
2014-06-09 20:08:08 +0200

    Support configuration of socket and connection time out.

--- flume-zipkin-collector-sink/src/main/java/com/github/kristofa/flume/ZipkinSpanCollectorSink.java
+++ flume-zipkin-collector-sink/src/main/java/com/github/kristofa/flume/ZipkinSpanCollectorSink.java
@@ -36,6 +36,8 @@ import com.twitter.zipkin.gen.ZipkinCollector;
  * <li>hostname: Host name for scribe source or zipkin collector. Mandatory, no default value.</li>
  * <li>port: Port for scribe source of zipkin collector. Mandatory, no default value.</li>
  * <li>batchsize: How many event should be sent at once if there are that many available. Optional, default value = 100.</li>
+ * <li>timeout: Time out value will be used both as initial connection time out and as SO_TIMEOUT value. Thrift doesn't allow
+ * configuring both separately. Optional, default value = 2000 ms.</li>
  * </ul>
  * 
  * @author adriaens
@@ -46,8 +48,10 @@ public class ZipkinSpanCollectorSink extends AbstractSink implements Configurabl
     private static final String PORT_CONFIG_PROP_NAME = "port";
     private static final String HOSTNAME_CONFIG_PROP_NAME = "hostname";
     private static final String BATCH_SIZE_PROP_NAME = "batchsize";
+    private static final String TIMEOUT_PROP_NAME = "timeout";
     private static final String SCRIBE_CATEGORY = "category";
     private static final int DEFAULT_BATCH_SIZE = 100;
+    private static final int DEFAULT_TIME_OUT = 2000;
 
     private TTransport transport;
     private ZipkinCollector.Client client;
@@ -56,6 +60,7 @@ public class ZipkinSpanCollectorSink extends AbstractSink implements Configurabl
     private LifecycleState lifeCycleState;
     private SinkCounter sinkCounter;
     private int batchSize = DEFAULT_BATCH_SIZE;
+    private int timeOut = DEFAULT_TIME_OUT;
 
     /**
      * {@inheritDoc}
@@ -101,7 +106,7 @@ public class ZipkinSpanCollectorSink extends AbstractSink implements Configurabl
     @Override
     public Status process() throws EventDeliveryException {
 
-        Status status = Status.BACKOFF;
+        Status status = Status.READY;
         final Channel channel = getChannel();
         final Transaction txn = channel.getTransaction();
         txn.begin();
@@ -118,9 +123,9 @@ public class ZipkinSpanCollectorSink extends AbstractSink implements Configurabl
 
                 client.Log(logEntries);
                 sinkCounter.incrementBatchCompleteCount();
-                status = Status.READY;
             } else {
                 sinkCounter.incrementBatchEmptyCount();
+                status = Status.BACKOFF;
             }
             txn.commit();
         } catch (final TTransportException e) {
@@ -131,6 +136,7 @@ public class ZipkinSpanCollectorSink extends AbstractSink implements Configurabl
                 LOGGER.info("Reconnect succeeded.");
             } catch (final TTransportException e1) {
                 LOGGER.error("Trying to reconnect failed.", e1);
+                throw new EventDeliveryException(e1);
             }
         } catch (final Throwable e) {
             txn.rollback();
@@ -150,12 +156,14 @@ public class ZipkinSpanCollectorSink extends AbstractSink implements Configurabl
         hostName = context.getString(HOSTNAME_CONFIG_PROP_NAME);
         port = context.getInteger(PORT_CONFIG_PROP_NAME);
         batchSize = context.getInteger(BATCH_SIZE_PROP_NAME, DEFAULT_BATCH_SIZE);
+        timeOut = context.getInteger(TIMEOUT_PROP_NAME, DEFAULT_TIME_OUT);
 
         if (sinkCounter == null) {
             sinkCounter = new SinkCounter(getName());
         }
 
-        LOGGER.info("Configuring ZipkinSpanCollectorSink. hostname: {}, port: {}, batchsize: {}", hostName, port, batchSize);
+        LOGGER.info("Configuring ZipkinSpanCollectorSink. hostname: {}, port: {}, batchsize: {}, timeout: {}", hostName,
+            port, batchSize, timeOut);
     }
 
     private LogEntry create(final Event event) {
@@ -171,7 +179,7 @@ public class ZipkinSpanCollectorSink extends AbstractSink implements Configurabl
         if (transport != null) {
             transport.close();
         }
-        transport = new TFramedTransport(new TSocket(hostName, port));
+        transport = new TFramedTransport(new TSocket(hostName, port, timeOut));
         final TProtocol protocol = new TBinaryProtocol(transport);
         client = new ZipkinCollector.Client(protocol);
         try {
