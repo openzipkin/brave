c4002a6dcdebe09b0e320d6a6a72122f9708d61f
David Schlosnagle <schlosna@gmail.com>
2016-10-01 04:20:23 -0400

    Improve brave-http request/response adapters code consistency (#246)
    Shows minor performance improvement:
    
    Benchmark   Mode  Cnt   Score   Error  Units
    Before      avgt   15  65.362 ± 3.308  ns/op
    After       avgt   15  48.170 ± 2.641  ns/op

--- brave-http/src/main/java/com/github/kristofa/brave/http/HttpClientRequestAdapter.java
+++ brave-http/src/main/java/com/github/kristofa/brave/http/HttpClientRequestAdapter.java
@@ -8,9 +8,8 @@ import com.github.kristofa.brave.internal.Nullable;
 import com.twitter.zipkin.gen.Endpoint;
 import zipkin.TraceKeys;
 
-import java.net.URI;
-import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 
 public class HttpClientRequestAdapter implements ClientRequestAdapter {
 
@@ -21,7 +20,6 @@ public class HttpClientRequestAdapter implements ClientRequestAdapter {
         this.request = request;
         this.spanNameProvider = spanNameProvider;
     }
-
 
     @Override
     public String getSpanName() {
@@ -42,16 +40,15 @@ public class HttpClientRequestAdapter implements ClientRequestAdapter {
         }
     }
 
-
     @Override
     public Collection<KeyValueAnnotation> requestAnnotations() {
-        URI uri = request.getUri();
-        KeyValueAnnotation annotation = KeyValueAnnotation.create(TraceKeys.HTTP_URL, uri.toString());
-        return Arrays.asList(annotation);
+        return Collections.singleton(KeyValueAnnotation.create(
+                TraceKeys.HTTP_URL, request.getUri().toString()));
     }
 
     @Override
     public Endpoint serverAddress() {
         return null;
     }
+
 }
