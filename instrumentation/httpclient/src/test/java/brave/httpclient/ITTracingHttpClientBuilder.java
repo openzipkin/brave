ac761620c987a7313d092a98fb422272924a6a40
Adrian Cole <acole@pivotal.io>
2017-05-17 14:28:06 +0800

    Eagerly provisions span so it is visible to httpclient interceptors
    Thanks @TilinC for reporting

--- instrumentation/httpclient/src/test/java/brave/httpclient/ITTracingHttpClientBuilder.java
+++ instrumentation/httpclient/src/test/java/brave/httpclient/ITTracingHttpClientBuilder.java
@@ -3,11 +3,17 @@ package brave.httpclient;
 import brave.http.ITHttpClient;
 import java.io.IOException;
 import java.net.URI;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.RecordedRequest;
+import org.apache.http.HttpRequestInterceptor;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.junit.AssumptionViolatedException;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ITTracingHttpClientBuilder extends ITHttpClient<CloseableHttpClient> {
 
@@ -33,5 +39,21 @@ public class ITTracingHttpClientBuilder extends ITHttpClient<CloseableHttpClient
 
   @Override protected void getAsync(CloseableHttpClient client, String pathIncludingQuery) {
     throw new AssumptionViolatedException("This is not an async library");
+  }
+
+  @Test public void currentSpanVisibleToUserFilters() throws Exception {
+    server.enqueue(new MockResponse());
+    closeClient(client);
+
+    client = TracingHttpClientBuilder.create(httpTracing).disableAutomaticRetries()
+        .addInterceptorFirst((HttpRequestInterceptor) (request, context) ->
+            request.setHeader("my-id", currentTraceContext.get().traceIdString())
+        ).build();
+
+    get(client, "/foo");
+
+    RecordedRequest request = server.takeRequest();
+    assertThat(request.getHeader("x-b3-traceId"))
+        .isEqualTo(request.getHeader("my-id"));
   }
 }
