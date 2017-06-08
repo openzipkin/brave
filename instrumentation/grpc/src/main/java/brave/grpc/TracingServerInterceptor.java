46904f7555d904116100d1326fe9023cca1444bf
Adrian Cole <acole@pivotal.io>
2017-06-08 11:09:04 +0800

    Ensures gRPC implementations can see the current span
    Before, we handled user interceptors, but not the server implementation.
    
    Fixes #425

--- instrumentation/grpc/src/main/java/brave/grpc/TracingServerInterceptor.java
+++ instrumentation/grpc/src/main/java/brave/grpc/TracingServerInterceptor.java
@@ -6,8 +6,8 @@ import brave.Tracing;
 import brave.propagation.Propagation;
 import brave.propagation.TraceContext;
 import brave.propagation.TraceContextOrSamplingFlags;
-import io.grpc.ForwardingServerCall;
 import io.grpc.ForwardingServerCall.SimpleForwardingServerCall;
+import io.grpc.ForwardingServerCallListener.SimpleForwardingServerCallListener;
 import io.grpc.Metadata;
 import io.grpc.ServerCall;
 import io.grpc.ServerCallHandler;
@@ -37,36 +37,80 @@ final class TracingServerInterceptor implements ServerInterceptor {
     Span span = contextOrFlags.context() != null
         ? tracer.joinSpan(contextOrFlags.context())
         : tracer.newTrace(contextOrFlags.samplingFlags());
+    span.kind(Span.Kind.SERVER).name(call.getMethodDescriptor().getFullMethodName());
+
+    // startCall invokes user interceptors, so we place the span in scope here
+    ServerCall.Listener<ReqT> result;
     try (Tracer.SpanInScope ws = tracer.withSpanInScope(span)) {
-      return next.startCall(new TracingServerCall<>(span, call), requestHeaders);
+      result = next.startCall(new TracingServerCall<>(span, call), requestHeaders);
     }
+
+    // This ensures the server implementation can see the span in scope
+    return new ScopingServerCallListener<>(tracer, span, result);
   }
 
   final class TracingServerCall<ReqT, RespT> extends SimpleForwardingServerCall<ReqT, RespT> {
     private final Span span;
-    private final ServerCall<ReqT, RespT> call;
 
     TracingServerCall(Span span, ServerCall<ReqT, RespT> call) {
       super(call);
       this.span = span;
-      this.call = call;
     }
 
     @Override public void request(int numMessages) {
-      try (Tracer.SpanInScope ws = tracer.withSpanInScope(span)) {
-        span.kind(Span.Kind.SERVER).name(call.getMethodDescriptor().getFullMethodName()).start();
-        super.request(numMessages);
-      }
+      span.start();
+      super.request(numMessages);
     }
 
     @Override public void close(Status status, Metadata trailers) {
-      try (Tracer.SpanInScope ws = tracer.withSpanInScope(span)) {
+      try {
         if (!status.getCode().equals(Status.Code.OK)) {
           span.tag(Constants.ERROR, String.valueOf(status.getCode()));
         }
         super.close(status, trailers);
       } finally {
         span.finish();
+      }
+    }
+  }
+
+  final class ScopingServerCallListener<ReqT> extends SimpleForwardingServerCallListener<ReqT> {
+    final Tracer tracer;
+    final Span span;
+
+    ScopingServerCallListener(Tracer tracer, Span span, ServerCall.Listener<ReqT> delegate) {
+      super(delegate);
+      this.tracer = tracer;
+      this.span = span;
+    }
+
+    @Override public void onMessage(ReqT message) {
+      try (Tracer.SpanInScope ws = tracer.withSpanInScope(span)) {
+        delegate().onMessage(message);
+      }
+    }
+
+    @Override public void onHalfClose() {
+      try (Tracer.SpanInScope ws = tracer.withSpanInScope(span)) {
+        delegate().onHalfClose();
+      }
+    }
+
+    @Override public void onCancel() {
+      try (Tracer.SpanInScope ws = tracer.withSpanInScope(span)) {
+        delegate().onCancel();
+      }
+    }
+
+    @Override public void onComplete() {
+      try (Tracer.SpanInScope ws = tracer.withSpanInScope(span)) {
+        delegate().onCancel();
+      }
+    }
+
+    @Override public void onReady() {
+      try (Tracer.SpanInScope ws = tracer.withSpanInScope(span)) {
+        delegate().onReady();
       }
     }
   }
