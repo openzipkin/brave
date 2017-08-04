00ad52c38775407ed24b66f3b6224de22a528d7b
Adrian Cole <adriancole@users.noreply.github.com>
2017-08-04 19:34:53 +0800

    Updates to Zipkin 1.29 (#465)
    This works in support of Messaging spans, though docs and examples will
    be follow-up work.

--- brave/src/main/java/brave/internal/recorder/MutableSpan.java
+++ brave/src/main/java/brave/internal/recorder/MutableSpan.java
@@ -3,53 +3,30 @@ package brave.internal.recorder;
 import brave.Span;
 import brave.propagation.TraceContext;
 import javax.annotation.Nullable;
-import zipkin.Annotation;
-import zipkin.BinaryAnnotation;
-import zipkin.Constants;
 import zipkin.Endpoint;
-
-import static zipkin.Constants.LOCAL_COMPONENT;
+import zipkin.internal.Span2;
 
 final class MutableSpan {
-  final Endpoint localEndpoint;
-  final zipkin.Span.Builder span;
-  boolean shared;
-  // fields which are added late
-  long startTimestamp;
-  Endpoint remoteEndpoint;
-
-  // flags which help us know how to reassemble the span
-  Span.Kind kind;
-
-  int flags;
-  static final int FLAG_CS = 1 << 0;
-  static final int FLAG_SR = 1 << 1;
-  static final int FLAG_SS = 1 << 2;
-  static final int FLAG_CR = 1 << 3;
-  static final int FLAG_LOCAL_ENDPOINT = 1 << 4;
-
+  final Span2.Builder span;
   boolean finished;
+  long timestamp;
 
   // Since this is not exposed, this class could be refactored later as needed to act in a pool
   // to reduce GC churn. This would involve calling span.clear and resetting the fields below.
   MutableSpan(TraceContext context, Endpoint localEndpoint) {
-    this.localEndpoint = localEndpoint;
-    this.span = zipkin.Span.builder()
+    this.span = Span2.builder()
         .traceIdHigh(context.traceIdHigh())
         .traceId(context.traceId())
         .parentId(context.parentId())
         .id(context.spanId())
         .debug(context.debug())
-        .name(""); // avoid a NPE
-    shared = context.shared();
-    startTimestamp = 0;
-    remoteEndpoint = null;
-    kind = null;
+        .shared(context.shared())
+        .localEndpoint(localEndpoint);
     finished = false;
   }
 
   synchronized MutableSpan start(long timestamp) {
-    startTimestamp = timestamp;
+    span.timestamp(this.timestamp = timestamp);
     return this;
   }
 
@@ -59,38 +36,26 @@ final class MutableSpan {
   }
 
   synchronized MutableSpan kind(Span.Kind kind) {
-    this.kind = kind;
-    return this;
-  }
-
-  synchronized MutableSpan annotate(long timestamp, String value) {
-    span.addAnnotation(Annotation.create(timestamp, value, localEndpoint));
-    flags |= FLAG_LOCAL_ENDPOINT;
-    if (value.length() != 2) return this;
-    if (value.equals(Constants.CLIENT_SEND)) {
-      flags |= FLAG_CS;
-      kind = Span.Kind.CLIENT;
-    } else if (value.equals(Constants.SERVER_RECV)) {
-      flags |= FLAG_SR;
-      kind = Span.Kind.SERVER;
-    } else if (value.equals(Constants.SERVER_SEND)) {
-      flags |= FLAG_SS;
-      kind = Span.Kind.SERVER;
-    } else if (value.equals(Constants.CLIENT_RECV)) {
-      flags |= FLAG_CR;
-      kind = Span.Kind.CLIENT;
+    try {
+      span.kind(Span2.Kind.valueOf(kind.name()));
+    } catch (IllegalArgumentException e) {
+      // TODO: log
     }
     return this;
   }
 
+  synchronized MutableSpan annotate(long timestamp, String value) {
+    span.addAnnotation(timestamp, value);
+    return this;
+  }
+
   synchronized MutableSpan tag(String key, String value) {
-    span.addBinaryAnnotation(BinaryAnnotation.create(key, value, localEndpoint));
-    flags |= FLAG_LOCAL_ENDPOINT;
+    span.putTag(key, value);
     return this;
   }
 
   synchronized MutableSpan remoteEndpoint(Endpoint remoteEndpoint) {
-    this.remoteEndpoint = remoteEndpoint;
+    span.remoteEndpoint(remoteEndpoint);
     return this;
   }
 
@@ -99,59 +64,13 @@ final class MutableSpan {
     if (finished) return this;
     finished = true;
 
-    if (startTimestamp != 0) {
-      span.timestamp(startTimestamp);
-      if (finishTimestamp != null) {
-        span.duration(Math.max(finishTimestamp - startTimestamp, 1));
-      }
-    }
-    if (kind != null) {
-      String remoteEndpointType;
-      String startAnnotation;
-      String finishAnnotation;
-      switch (kind) {
-        case CLIENT:
-          remoteEndpointType = Constants.SERVER_ADDR;
-          startAnnotation = (flags & FLAG_CS) == 0 ? Constants.CLIENT_SEND : null;
-          finishAnnotation = (flags & FLAG_CR) == 0 ? Constants.CLIENT_RECV : null;
-          break;
-        case SERVER:
-          remoteEndpointType = Constants.CLIENT_ADDR;
-          startAnnotation = (flags & FLAG_SR) == 0 ? Constants.SERVER_RECV : null;
-          finishAnnotation = (flags & FLAG_SS) == 0 ? Constants.SERVER_SEND : null;
-          break;
-        default:
-          throw new AssertionError("update kind mapping");
-      }
-      if (remoteEndpoint != null) {
-        span.addBinaryAnnotation(BinaryAnnotation.address(remoteEndpointType, remoteEndpoint));
-      }
-      if (startAnnotation != null && startTimestamp != 0) {
-        if (startAnnotation.equals(Constants.SERVER_RECV)) flags |= FLAG_SR;
-        if (startAnnotation.equals(Constants.CLIENT_SEND)) flags |= FLAG_CS;
-        span.addAnnotation(Annotation.create(startTimestamp, startAnnotation, localEndpoint));
-      }
-      if (finishAnnotation != null && finishTimestamp != null) {
-        span.addAnnotation(Annotation.create(finishTimestamp, finishAnnotation, localEndpoint));
-      }
-      flags |= FLAG_LOCAL_ENDPOINT;
-    }
-    // don't report server-side timestamp on shared or incomplete spans
-    if (shared && (flags & FLAG_SR) != 0) {
-      span.timestamp(null).duration(null);
-    }
-    // don't report client span.timestamp if unfinished.
-    // This allows one-way to be modeled as span.kind(serverOrClient).start().flush()
-    if ((flags & (FLAG_CS | FLAG_SR)) != 0 && finishTimestamp == null) {
-      span.timestamp(null);
-    }
-    if ((flags & FLAG_LOCAL_ENDPOINT) == 0) { // create a small dummy annotation
-      span.addBinaryAnnotation(BinaryAnnotation.create(LOCAL_COMPONENT, "", localEndpoint));
+    if (timestamp != 0 && finishTimestamp != null) {
+      span.duration(Math.max(finishTimestamp - timestamp, 1));
     }
     return this;
   }
 
-  synchronized zipkin.Span toSpan() {
+  synchronized Span2 toSpan() {
     return span.build();
   }
 }
