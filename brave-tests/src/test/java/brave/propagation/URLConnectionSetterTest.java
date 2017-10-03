a362bb97094df413797dc0463c326c7fb7936b7f
Adrian Cole <acole@pivotal.io>
2017-10-03 18:23:36 +0800

    Adds missing test

---
+++ brave-tests/src/test/java/brave/propagation/URLConnectionSetterTest.java
@@ -0,0 +1,33 @@
+package brave.propagation;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URLConnection;
+
+/** Example setter test */
+public class URLConnectionSetterTest extends PropagationSetterTest<URLConnection, String> {
+  final URLConnection carrier;
+
+  public URLConnectionSetterTest() throws IOException {
+    carrier = new URLConnection(URI.create("http://127.0.0.1:9999").toURL()) {
+      @Override public void connect() throws IOException {
+      }
+    };
+  }
+
+  @Override public Propagation.KeyFactory<String> keyFactory() {
+    return Propagation.KeyFactory.STRING;
+  }
+
+  @Override protected URLConnection carrier() {
+    return carrier;
+  }
+
+  @Override protected Propagation.Setter<URLConnection, String> setter() {
+    return URLConnection::setRequestProperty;
+  }
+
+  @Override protected Iterable<String> read(URLConnection carrier, String key) {
+    return carrier.getRequestProperties().get(key);
+  }
+}
