7a34d07f047e391abe3457c0d85c89cec1dd33db
Ivan_Tamashevich <Ivan_Tamashevich@epam.com>
2017-11-01 12:38:04 +0300

    Decreasing Java level to 6. More classic formatting added.

--- archive/brave-web-servlet-filter/src/test/java/com/github/kristofa/brave/servlet/ServletHttpServerRequestUnitTest.java
+++ archive/brave-web-servlet-filter/src/test/java/com/github/kristofa/brave/servlet/ServletHttpServerRequestUnitTest.java
@@ -1,36 +1,45 @@
 package com.github.kristofa.brave.servlet;
 
+import org.junit.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import javax.servlet.http.HttpServletRequest;
+
 import static org.junit.Assert.fail;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import javax.servlet.http.HttpServletRequest;
 
-import org.junit.Test;
-import org.mockito.stubbing.Answer;
-
-
-public class ServletHttpServerRequestUnitTest
-{
+public class ServletHttpServerRequestUnitTest {
 
     @Test
     public void passWithNonEncodedCharacters() {
         final HttpServletRequest request = mock(HttpServletRequest.class);
 
-        when(request.getRequestURL()).thenAnswer((Answer<StringBuffer>) invocation ->
-           new StringBuffer("https://мойДядя.самых/честных->правил/когда[Не]В~Шутку|Занемог/он УважатьСебяЗаставил")
+        when(request.getRequestURL()).thenAnswer(
+                new Answer<StringBuffer>() {
+                    @Override
+                    public StringBuffer answer(InvocationOnMock invocationOnMock) throws Throwable {
+                        return new StringBuffer("https://мойДядя.самых/честных->правил/когда[Не]В~Шутку|Занемог/он УважатьСебяЗаставил");
+                    }
+                }
         );
 
-        when(request.getQueryString()).thenAnswer((Answer<String>) invocation ->
-              "И=Лучше&Выдумать=Не мог"
+        when(request.getQueryString()).thenAnswer(
+                new Answer<String>() {
+                    @Override
+                    public String answer(InvocationOnMock invocationOnMock) throws Throwable {
+                        return "И=Лучше&Выдумать=Не мог";
+                    }
+                }
         );
 
         ServletHttpServerRequest shsr = new ServletHttpServerRequest(request);
 
-        try{
+        try {
             shsr.getUri();
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             fail("Should not have thrown any exception, although got: " + e.getMessage());
         }
 
