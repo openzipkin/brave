4e0e251d480f07b74ad12cd1f0a26eb443d8596f
Adrian Cole <acole@pivotal.io>
2017-12-07 09:36:45 -0800

    Makes end-to-end benchmarks more realistic
    Chat with @wu-sheng made me realize our end-to-end benchmarks were not
    that realistic, on account of using a noop reporter. Most people use
    `AsyncReporter` and eventhough that code lives outside Brave, it is a
    very important factor in latency which should be included.
    
    For example, here's with 1KiB message size (to avoid GC thrash in JMH)
    ```
    Benchmark                                           Mode  Cnt    Score    Error  Units
    EndToEndBenchmarks.server_get                       avgt   15  159.013 ±  9.592  us/op
    EndToEndBenchmarks.traced128Server_get              avgt   15  223.974 ± 14.517  us/op
    EndToEndBenchmarks.traced128Server_get_resumeTrace  avgt   15  226.344 ±  9.706  us/op
    EndToEndBenchmarks.tracedServer_get                 avgt   15  220.218 ±  6.028  us/op
    EndToEndBenchmarks.tracedServer_get_resumeTrace     avgt   15  218.240 ±  2.942  us/op
    EndToEndBenchmarks.tracedawsServer_get              avgt   15  218.895 ±  6.939  us/op
    EndToEndBenchmarks.tracedawsServer_get_resumeTrace  avgt   15  219.882 ± 12.913  us/op
    EndToEndBenchmarks.unsampledServer_get              avgt   15  166.991 ±  2.342  us/op
    ```
    
    Overhead without the async reporter is usually up to 30us. The above
    shows significant opportunity to reduce overhead there. FWIW, it is
    mainly due to encoding spans on the calling thread. There are other
    ways to accomplish the goal of memory-bounding now that the data
    structures are simpler (in zipkin v2).

---
+++ instrumentation/benchmarks/src/main/java/brave/NoopSender.java
@@ -0,0 +1,46 @@
+package brave;
+
+import java.util.List;
+import zipkin2.Call;
+import zipkin2.CheckResult;
+import zipkin2.codec.Encoding;
+import zipkin2.reporter.BytesMessageEncoder;
+import zipkin2.reporter.Sender;
+
+final class NoopSender extends Sender {
+
+  final Encoding encoding = Encoding.JSON;
+  final BytesMessageEncoder messageEncoder = BytesMessageEncoder.forEncoding(encoding);
+
+  /** close is typically called from a different thread */
+  volatile boolean closeCalled;
+
+  @Override public int messageMaxBytes() {
+    return 1024;
+  }
+
+  @Override public Encoding encoding() {
+    return encoding;
+  }
+
+  @Override public int messageSizeInBytes(List<byte[]> encodedSpans) {
+    return encoding().listSizeInBytes(encodedSpans);
+  }
+
+  @Override public Call<Void> sendSpans(List<byte[]> encodedSpans) {
+    messageEncoder.encode(encodedSpans);
+    return Call.create(null);
+  }
+
+  @Override public CheckResult check() {
+    return CheckResult.OK;
+  }
+
+  @Override public void close() {
+    closeCalled = true;
+  }
+
+  @Override public String toString() {
+    return "NoopSender";
+  }
+}
