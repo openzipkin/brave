7c0b607d6230f5682ecdf5e03a29d9954f3a6fd5
Adrian Cole <acole@pivotal.io>
2017-12-15 15:06:02 +0800

    Corrects default encoding in spring-beans
    While our old bean factory defaulted to json v1, the new one doesn't.

--- spring-beans/src/main/java/brave/spring/beans/AsyncReporterFactoryBean.java
+++ spring-beans/src/main/java/brave/spring/beans/AsyncReporterFactoryBean.java
@@ -1,6 +1,12 @@
 package brave.spring.beans;
 
+import zipkin2.codec.SpanBytesEncoder;
+
 /** @deprecated use {@link zipkin2.reporter.beans.AsyncReporterFactoryBean} */
 @Deprecated
 public class AsyncReporterFactoryBean extends zipkin2.reporter.beans.AsyncReporterFactoryBean {
+  public AsyncReporterFactoryBean() {
+    // This bean factory defaulted to the old format in the past. The new doesn't
+    setEncoder(SpanBytesEncoder.JSON_V1);
+  }
 }
