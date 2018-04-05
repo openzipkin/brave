c44c136e0a2d18fbe763e6361b2273a1fde94e08
Adrian Cole <acole@pivotal.io>
2018-04-05 09:08:06 +0800

    Removes v1 zipkin imports

--- brave/src/main/java/brave/SpanCustomizer.java
+++ brave/src/main/java/brave/SpanCustomizer.java
@@ -1,8 +1,5 @@
 package brave;
 
-import zipkin.Constants;
-import zipkin.TraceKeys;
-
 /**
  * Simple interface users can customize a span with. For example, this can add custom tags useful
  * in looking up spans.
@@ -32,11 +29,10 @@ public interface SpanCustomizer {
 
   /**
    * Tags give your span context for search, viewing and analysis. For example, a key
-   * "your_app.version" would let you lookup spans by version. A tag {@link TraceKeys#SQL_QUERY}
-   * isn't searchable, but it can help in debugging when viewing a trace.
+   * "your_app.version" would let you lookup spans by version. A tag "sql.query" isn't searchable,
+   * but it can help in debugging when viewing a trace.
    *
-   * @param key Name used to lookup spans, such as "your_app.version". See {@link TraceKeys} for
-   * standard ones.
+   * @param key Name used to lookup spans, such as "your_app.version".
    * @param value String value, cannot be <code>null</code>.
    */
   SpanCustomizer tag(String key, String value);
@@ -45,7 +41,6 @@ public interface SpanCustomizer {
    * Associates an event that explains latency with the current system time.
    *
    * @param value A short tag indicating the event, like "finagle.retry"
-   * @see Constants
    */
   SpanCustomizer annotate(String value);
 
