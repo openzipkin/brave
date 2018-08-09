ba22be4913f985c40e775fe51f2426b1d36ee385
Jorge Quilcate Otoya <quilcate.jorge@gmail.com>
2018-08-09 11:31:48 +0200

    feat: upgrade kafka clients to v2.0

--- instrumentation/kafka-clients/src/main/java/brave/kafka/clients/TracingConsumer.java
+++ instrumentation/kafka-clients/src/main/java/brave/kafka/clients/TracingConsumer.java
@@ -6,6 +6,8 @@ import brave.internal.Nullable;
 import brave.propagation.TraceContext.Extractor;
 import brave.propagation.TraceContext.Injector;
 import brave.propagation.TraceContextOrSamplingFlags;
+
+import java.time.Duration;
 import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -48,8 +50,12 @@ final class TracingConsumer<K, V> implements Consumer<K, V> {
     this.remoteServiceName = remoteServiceName;
   }
 
+  @Deprecated @Override public ConsumerRecords<K, V> poll(long timeout) {
+    return poll(Duration.ofMillis(timeout));
+  }
+
   /** This */
-  @Override public ConsumerRecords<K, V> poll(long timeout) {
+  @Override public ConsumerRecords<K, V> poll(Duration timeout) {
     ConsumerRecords<K, V> records = delegate.poll(timeout);
     if (records.isEmpty() || tracing.isNoop()) return records;
     Map<String, Span> consumerSpansForTopic = new LinkedHashMap<>();
@@ -134,8 +140,16 @@ final class TracingConsumer<K, V> implements Consumer<K, V> {
     delegate.commitSync();
   }
 
+  @Override public void commitSync(Duration timeout) {
+    delegate.commitSync(timeout);
+  }
+
   @Override public void commitSync(Map<TopicPartition, OffsetAndMetadata> offsets) {
     delegate.commitSync(offsets);
+  }
+
+  @Override public void commitSync(Map<TopicPartition, OffsetAndMetadata> offsets, Duration timeout) {
+    delegate.commitSync(offsets, timeout);
   }
 
   @Override public void commitAsync() {
@@ -167,8 +181,16 @@ final class TracingConsumer<K, V> implements Consumer<K, V> {
     return delegate.position(partition);
   }
 
+  @Override public long position(TopicPartition partition, Duration timeout) {
+    return delegate.position(partition, timeout);
+  }
+
   @Override public OffsetAndMetadata committed(TopicPartition partition) {
     return delegate.committed(partition);
+  }
+
+  @Override public OffsetAndMetadata committed(TopicPartition partition, Duration timeout) {
+    return delegate.committed(partition, timeout);
   }
 
   @Override public Map<MetricName, ? extends Metric> metrics() {
@@ -179,8 +201,16 @@ final class TracingConsumer<K, V> implements Consumer<K, V> {
     return delegate.partitionsFor(topic);
   }
 
+  @Override public List<PartitionInfo> partitionsFor(String topic, Duration timeout) {
+    return delegate.partitionsFor(topic, timeout);
+  }
+
   @Override public Map<String, List<PartitionInfo>> listTopics() {
     return delegate.listTopics();
+  }
+
+  @Override public Map<String, List<PartitionInfo>> listTopics(Duration timeout) {
+    return delegate.listTopics(timeout);
   }
 
   @Override public Set<TopicPartition> paused() {
@@ -200,13 +230,24 @@ final class TracingConsumer<K, V> implements Consumer<K, V> {
     return delegate.offsetsForTimes(timestampsToSearch);
   }
 
-  @Override
-  public Map<TopicPartition, Long> beginningOffsets(Collection<TopicPartition> partitions) {
+  @Override public Map<TopicPartition, OffsetAndTimestamp> offsetsForTimes(Map<TopicPartition, Long> timestampsToSearch, Duration timeout) {
+    return delegate.offsetsForTimes(timestampsToSearch, timeout);
+  }
+
+  @Override public Map<TopicPartition, Long> beginningOffsets(Collection<TopicPartition> partitions) {
     return delegate.beginningOffsets(partitions);
+  }
+
+  @Override public Map<TopicPartition, Long> beginningOffsets(Collection<TopicPartition> partitions, Duration timeout) {
+    return delegate.beginningOffsets(partitions, timeout);
   }
 
   @Override public Map<TopicPartition, Long> endOffsets(Collection<TopicPartition> partitions) {
     return delegate.endOffsets(partitions);
+  }
+
+  @Override public Map<TopicPartition, Long> endOffsets(Collection<TopicPartition> partitions, Duration timeout) {
+    return delegate.endOffsets(partitions, timeout);
   }
 
   @Override public void close() {
@@ -215,6 +256,10 @@ final class TracingConsumer<K, V> implements Consumer<K, V> {
 
   @Override public void close(long timeout, TimeUnit unit) {
     delegate.close(timeout, unit);
+  }
+
+  @Override public void close(Duration timeout) {
+     delegate.close(timeout);
   }
 
   @Override public void wakeup() {
