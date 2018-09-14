openzipkin/brave

commit ca2ec95af85e3701d30d292c4a9918c3aa0eb78e

Ensures extra fields are not accidentally cleared when trace IDs are
There is some defensive logic in messaging to prevent re-processing the
same trace. This accidentally cleared extra fields which made it
impossible for folks to access them via headers later.

Fixes #783

brave/src/main/java/brave/propagation/ExtraFieldPropagation.java
@@ -258,20 +258,22 @@ public final class ExtraFieldPropagation<K> implements Propagation<K> {
   final Propagation<K> delegate;
   final ExtraFactory extraFactory;
   final String[] fieldNames;
-  final List<K> keys, allKeys;
+  final List<K> keys;
 
   ExtraFieldPropagation(Factory factory, Propagation.KeyFactory<K> keyFactory, List<K> keys) {
     this.delegate = factory.delegate.create(keyFactory);
     this.extraFactory = factory.extraFactory;
     this.fieldNames = factory.fieldNames;
     this.keys = keys;
-    List<K> allKeys = new ArrayList<>(delegate.keys());
-    allKeys.addAll(keys);
-    this.allKeys = allKeys;
   }
 
+  /**
+   * Only returns trace context keys. Extra field names are not returned to ensure tools don't
+   * delete them. This is to support users accessing extra fields without Brave apis (ex via
+   * headers).
+   */
   @Override public List<K> keys() {
-    return allKeys;
+    return delegate.keys();
   }
 
   @Override public <C> Injector<C> injector(Setter<C, K> setter) {

brave/src/test/java/brave/propagation/ExtraFieldPropagationTest.java
@@ -35,6 +35,15 @@ public class ExtraFieldPropagationTest {
         .build());
   }
 
+  /**
+   * Ensure extra fields aren't leaked. This prevents tools from deleting entries when clearing a
+   * trace.
+   */
+  @Test public void keysDontIncludeExtra() {
+    assertThat(factory.create(Propagation.KeyFactory.STRING).keys())
+        .isEqualTo(Propagation.B3_STRING.keys());
+  }
+
   @Test public void downcasesNames() {
     ExtraFieldPropagation.Factory factory =
         (ExtraFieldPropagation.Factory) ExtraFieldPropagation.newFactory(B3Propagation.FACTORY,

brave/src/main/java/brave/propagation/Propagation.java
@@ -92,6 +92,10 @@ public interface Propagation<K> {
    * <p>For example, if the carrier is a single-use or immutable request object, you don't need to
    * clear fields as they couldn't have been set before. If it is a mutable, retryable object,
    * successive calls should clear these fields first.
+   *
+   * <p><em>Note:</em> If your implementation carries "extra fields", such as correlation IDs, do
+   * not return the names of those fields here. If you do, they will be deleted, which can interfere
+   * with user headers.
    */
   // The use cases of this are:
   // * allow pre-allocation of fields, especially in systems like gRPC Metadata

instrumentation/kafka-clients/src/main/java/brave/kafka/clients/KafkaPropagation.java
@@ -22,7 +22,6 @@ final class KafkaPropagation {
 
   static final Injector<Headers> B3_SINGLE_INJECTOR = new Injector<Headers>() {
     @Override public void inject(TraceContext traceContext, Headers carrier) {
-      carrier.remove("b3");
       carrier.add("b3", writeB3SingleFormatWithoutParentIdAsBytes(traceContext));
     }
 

commit 2eed0dff6ab29fb5faac8e881d32b5d870b4a2f3

Exposes ExtraField keys for OpenTracing Baggage integration
Fixing ca2ec95af85e3701d30d292c4a9918c3aa0eb78e broke OpenTracing
baggage integration. The root cause of the break is that OpenTracing's
`TextMap` has no get-by-name functionality. That means adapters have to
scan to figure out what keys it might need. When we fixed the accidental
deletion of extra keys, we accidentally broke the OpenTracing
workaround. I've raised opentracing/opentracing-java#305 in attempts to
get rid of needing a workaround in the first place. However, this will
restore functionality.

brave/src/main/java/brave/propagation/ExtraFieldPropagation.java
@@ -70,7 +70,7 @@ import java.util.Map;
  */
 public final class ExtraFieldPropagation<K> implements Propagation<K> {
   /** Wraps an underlying propagation implementation, pushing one or more fields */
-  public static Propagation.Factory newFactory(Propagation.Factory delegate, String... fieldNames) {
+  public static Factory newFactory(Propagation.Factory delegate, String... fieldNames) {
     if (delegate == null) throw new NullPointerException("delegate == null");
     if (fieldNames == null) throw new NullPointerException("fieldNames == null");
     String[] validated = ensureLowerCase(Arrays.asList(fieldNames));
@@ -78,7 +78,7 @@ public final class ExtraFieldPropagation<K> implements Propagation<K> {
   }
 
   /** Wraps an underlying propagation implementation, pushing one or more fields */
-  public static Propagation.Factory newFactory(Propagation.Factory delegate,
+  public static Factory newFactory(Propagation.Factory delegate,
       Collection<String> fieldNames) {
     if (delegate == null) throw new NullPointerException("delegate == null");
     if (fieldNames == null) throw new NullPointerException("fieldNames == null");
@@ -219,7 +219,7 @@ public final class ExtraFieldPropagation<K> implements Propagation<K> {
     PropagationFields.put(context, lowercase(name), value, Extra.class);
   }
 
-  static final class Factory extends Propagation.Factory {
+  public static final class Factory extends Propagation.Factory {
     final Propagation.Factory delegate;
     final String[] fieldNames;
     final String[] keyNames;
@@ -240,7 +240,8 @@ public final class ExtraFieldPropagation<K> implements Propagation<K> {
       return delegate.requires128BitTraceId();
     }
 
-    @Override public final <K> Propagation<K> create(Propagation.KeyFactory<K> keyFactory) {
+    @Override
+    public final <K> ExtraFieldPropagation<K> create(Propagation.KeyFactory<K> keyFactory) {
       int length = fieldNames.length;
       List<K> keys = new ArrayList<>(length);
       for (int i = 0; i < length; i++) {
@@ -265,6 +266,16 @@ public final class ExtraFieldPropagation<K> implements Propagation<K> {
     this.extraFactory = factory.extraFactory;
     this.fieldNames = factory.fieldNames;
     this.keys = keys;
+  }
+
+  /**
+   * Returns the extra keys this component can extract. This result is lowercase and does not
+   * include any {@link #keys() trace context keys}.
+   */
+  // This is here to support extraction from carriers missing a get field by name function. The only
+  // known example is OpenTracing TextMap https://github.com/opentracing/opentracing-java/issues/305
+  public List<K> extraKeys() {
+    return keys;
   }
 
   /**

brave/src/test/java/brave/features/opentracing/BraveTracer.java
@@ -1,6 +1,8 @@
 package brave.features.opentracing;
 
 import brave.internal.Nullable;
+import brave.propagation.ExtraFieldPropagation;
+import brave.propagation.Propagation;
 import brave.propagation.TraceContext;
 import brave.propagation.TraceContextOrSamplingFlags;
 import io.opentracing.ScopeManager;
@@ -11,8 +13,11 @@ import io.opentracing.propagation.Format;
 import io.opentracing.propagation.TextMap;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
+import java.util.Set;
 
 final class BraveTracer implements Tracer {
 
@@ -22,13 +27,17 @@ final class BraveTracer implements Tracer {
   }
 
   final brave.Tracer tracer;
-  final List<String> propagationKeys;
+  final Set<String> allPropagationKeys;
   final TraceContext.Injector<TextMap> injector;
   final TraceContext.Extractor<TextMapView> extractor;
 
   BraveTracer(brave.Tracing tracing) {
     tracer = tracing.tracer();
-    propagationKeys = tracing.propagation().keys();
+    Propagation<String> propagation = tracing.propagation();
+    allPropagationKeys = lowercaseSet(propagation.keys());
+    if (propagation instanceof ExtraFieldPropagation) {
+      allPropagationKeys.addAll(((ExtraFieldPropagation<String>) propagation).extraKeys());
+    }
     injector = tracing.propagation().injector(TextMap::put);
     extractor = tracing.propagation().extractor(TextMapView::get);
   }
@@ -58,36 +67,51 @@ final class BraveTracer implements Tracer {
       throw new UnsupportedOperationException(format.toString());
     }
     TraceContextOrSamplingFlags extracted =
-        extractor.extract(new TextMapView(propagationKeys, (TextMap) carrier));
+        extractor.extract(new TextMapView(allPropagationKeys, (TextMap) carrier));
     TraceContext context = extracted.context() != null
         ? tracer.joinSpan(extracted.context()).context()
         : tracer.nextSpan(extracted).context();
     return new BraveSpanContext(context);
   }
 
-  /** Eventhough TextMap is named like Map, it doesn't have a retrieve-by-key method */
+  /**
+   * Eventhough TextMap is named like Map, it doesn't have a retrieve-by-key method.
+   *
+   * <p>See https://github.com/opentracing/opentracing-java/issues/305
+   */
   static final class TextMapView {
     final Iterator<Map.Entry<String, String>> input;
     final Map<String, String> cache = new LinkedHashMap<>();
-    final List<String> fields;
+    final Set<String> allPropagationKeys;
 
-    TextMapView(List<String> fields, TextMap input) {
-      this.fields = fields;
+    TextMapView(Set<String> allPropagationKeys, TextMap input) {
+      this.allPropagationKeys = allPropagationKeys;
       this.input = input.iterator();
     }
 
+    /** Performs case-insensitive lookup */
     @Nullable String get(String key) {
+      key = key.toLowerCase(Locale.ROOT);
       String result = cache.get(key);
       if (result != null) return result;
       while (input.hasNext()) {
         Map.Entry<String, String> next = input.next();
-        if (next.getKey().equals(key)) {
+        String keyInCarrier = next.getKey().toLowerCase(Locale.ROOT);
+        if (keyInCarrier.equals(key)) {
           return next.getValue();
-        } else if (fields.contains(next.getKey())) {
-          cache.put(next.getKey(), next.getValue());
+        } else if (allPropagationKeys.contains(keyInCarrier)) {
+          cache.put(keyInCarrier, next.getValue());
         }
       }
       return null;
     }
+  }
+
+  static Set<String> lowercaseSet(List<String> fields) {
+    Set<String> lcSet = new LinkedHashSet<>();
+    for (String f : fields) {
+      lcSet.add(f.toLowerCase(Locale.ROOT));
+    }
+    return lcSet;
   }
 }

brave/src/test/java/brave/features/opentracing/OpenTracingAdapterTest.java
@@ -1,6 +1,10 @@
 package brave.features.opentracing;
 
 import brave.Tracing;
+import brave.propagation.B3Propagation;
+import brave.propagation.ExtraFieldPropagation;
+import brave.propagation.StrictScopeDecorator;
+import brave.propagation.ThreadLocalCurrentTraceContext;
 import brave.propagation.TraceContext;
 import io.opentracing.propagation.Format;
 import io.opentracing.propagation.TextMapExtractAdapter;
@@ -17,16 +21,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.data.MapEntry.entry;
 
 /**
- * This shows how one might make an OpenTracing adapter for Brave, and how to navigate in and out
- * of the core concepts.
+ * This shows how one might make an OpenTracing adapter for Brave, and how to navigate in and out of
+ * the core concepts.
  */
 public class OpenTracingAdapterTest {
   List<zipkin2.Span> spans = new ArrayList<>();
-  Tracing brave = Tracing.newBuilder().spanReporter(spans::add).build();
+  Tracing brave = Tracing.newBuilder()
+      .currentTraceContext(ThreadLocalCurrentTraceContext.newBuilder()
+          .addScopeDecorator(StrictScopeDecorator.create())
+          .build())
+      .propagationFactory(ExtraFieldPropagation.newFactory(B3Propagation.FACTORY, "client-id"))
+      .spanReporter(spans::add).build();
+
   BraveTracer opentracing = BraveTracer.wrap(brave);
 
   @After public void close() {
-    Tracing.current().close();
+    brave.close();
   }
 
   @Test public void startWithOpenTracingAndFinishWithBrave() {
@@ -61,6 +71,7 @@ public class OpenTracingAdapterTest {
     map.put("X-B3-TraceId", "0000000000000001");
     map.put("X-B3-SpanId", "0000000000000002");
     map.put("X-B3-Sampled", "1");
+    map.put("Client-Id", "sammy");
 
     BraveSpanContext openTracingContext =
         opentracing.extract(Format.Builtin.HTTP_HEADERS, new TextMapExtractAdapter(map));
@@ -71,6 +82,9 @@ public class OpenTracingAdapterTest {
             .spanId(2L)
             .shared(true)
             .sampled(true).build());
+
+    assertThat(openTracingContext.baggageItems())
+        .containsExactly(entry("client-id", "sammy"));
   }
 
   @Test

brave/src/test/java/brave/propagation/ExtraFieldPropagationTest.java
@@ -16,7 +16,7 @@ public class ExtraFieldPropagationTest {
   String awsTraceId =
       "Root=1-67891233-abcdef012345678912345678;Parent=463ac35c9f6413ad;Sampled=1";
   String uuid = "f4308d05-2228-4468-80f6-92a8377ba193";
-  Propagation.Factory factory = ExtraFieldPropagation.newFactory(
+  ExtraFieldPropagation.Factory factory = ExtraFieldPropagation.newFactory(
       B3Propagation.FACTORY, "x-vcap-request-id", "x-amzn-trace-id"
   );
 
@@ -42,6 +42,14 @@ public class ExtraFieldPropagationTest {
   @Test public void keysDontIncludeExtra() {
     assertThat(factory.create(Propagation.KeyFactory.STRING).keys())
         .isEqualTo(Propagation.B3_STRING.keys());
+  }
+
+  /**
+   * Ensures OpenTracing 0.31 can read the extra keys, as its TextMap has no get by name function.
+   */
+  @Test public void extraKeysDontIncludeTraceContextKeys() {
+    assertThat(factory.create(Propagation.KeyFactory.STRING).extraKeys())
+        .containsExactly("x-vcap-request-id", "x-amzn-trace-id");
   }
 
   @Test public void downcasesNames() {