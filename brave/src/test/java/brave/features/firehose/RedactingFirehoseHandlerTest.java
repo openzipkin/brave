67a3dd56422175ae24dcf1b155c239804709abdf
Adrian Cole <acole@pivotal.io>
2018-09-24 20:00:58 -0400

    Shows how to do redaction

---
+++ brave/src/test/java/brave/features/firehose/RedactingFirehoseHandlerTest.java
@@ -0,0 +1,85 @@
+package brave.features.firehose;
+
+import brave.ScopedSpan;
+import brave.Tracing;
+import brave.firehose.FirehoseHandler;
+import brave.firehose.MutableSpan;
+import brave.firehose.MutableSpan.AnnotationUpdater;
+import brave.firehose.MutableSpan.TagUpdater;
+import brave.propagation.TraceContext;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.junit.After;
+import org.junit.Test;
+import zipkin2.Annotation;
+import zipkin2.Span;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.entry;
+
+/** One reason {@link brave.firehose.MutableSpan} is mutable is to support redaction */
+public class RedactingFirehoseHandlerTest {
+  static final Pattern SSN = Pattern.compile("[0-9]{3}\\-[0-9]{2}\\-[0-9]{4}");
+
+  enum ValueRedactor implements TagUpdater, AnnotationUpdater {
+    INSTANCE;
+
+    @Override public String update(String key, String value) {
+      return maybeUpdateValue(value);
+    }
+
+    @Override public String update(long timestamp, String value) {
+      return maybeUpdateValue(value);
+    }
+
+    /** Simple example of a replacement pattern, deleting entries which only include SSNs */
+    static String maybeUpdateValue(String value) {
+      Matcher matcher = SSN.matcher(value);
+      if (matcher.find()) {
+        String matched = matcher.group(0);
+        if (matched.equals(value)) return null;
+        return value.replace(matched, "xxx-xx-xxxx");
+      }
+      return value;
+    }
+  }
+
+  List<Span> spans = new ArrayList<>();
+  Tracing tracing = Tracing.newBuilder()
+      .addFirehoseHandler(new FirehoseHandler() {
+        @Override public boolean handle(TraceContext context, MutableSpan span) {
+          span.forEachTag(ValueRedactor.INSTANCE);
+          span.forEachAnnotation(ValueRedactor.INSTANCE);
+          return true;
+        }
+      })
+      .spanReporter(spans::add)
+      .build();
+
+  @After public void close() {
+    tracing.close();
+  }
+
+  @Test public void showRedaction() {
+    ScopedSpan span = tracing.tracer().startScopedSpan("auditor");
+    try {
+      span.tag("a", "1");
+      span.tag("b", "912-23-1433");
+      span.annotate("SSN=912-23-1433");
+      span.tag("c", "3");
+    } finally {
+      span.finish();
+    }
+
+    assertThat(spans.get(0).tags()).containsExactly(
+        entry("a", "1"),
+        // SSN tag was nuked
+        entry("c", "3")
+    );
+    assertThat(spans.get(0).annotations()).flatExtracting(Annotation::value).containsExactly(
+        "SSN=xxx-xx-xxxx"
+    );
+  }
+}
