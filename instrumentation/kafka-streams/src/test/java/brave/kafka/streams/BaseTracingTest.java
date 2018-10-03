cc4b84497f1254aa6e3a8fe595f2b2acbe016681
Jorge Quilcate Otoya <quilcate.jorge@gmail.com>
2018-10-03 20:06:01 +0200

    refactor: use mockito for processor context

--- instrumentation/kafka-streams/src/test/java/brave/kafka/streams/BaseTracingTest.java
+++ instrumentation/kafka-streams/src/test/java/brave/kafka/streams/BaseTracingTest.java
@@ -3,28 +3,21 @@ package brave.kafka.streams;
 import brave.Tracing;
 import brave.propagation.StrictScopeDecorator;
 import brave.propagation.ThreadLocalCurrentTraceContext;
-import java.io.File;
-import java.util.Map;
 import java.util.concurrent.ConcurrentLinkedDeque;
 import java.util.function.Function;
 import org.apache.kafka.common.header.Headers;
-import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.streams.KeyValue;
-import org.apache.kafka.streams.StreamsMetrics;
 import org.apache.kafka.streams.kstream.Transformer;
 import org.apache.kafka.streams.kstream.TransformerSupplier;
 import org.apache.kafka.streams.processor.AbstractProcessor;
-import org.apache.kafka.streams.processor.Cancellable;
 import org.apache.kafka.streams.processor.ProcessorContext;
 import org.apache.kafka.streams.processor.ProcessorSupplier;
-import org.apache.kafka.streams.processor.PunctuationType;
-import org.apache.kafka.streams.processor.Punctuator;
-import org.apache.kafka.streams.processor.StateRestoreCallback;
-import org.apache.kafka.streams.processor.StateStore;
 import org.apache.kafka.streams.processor.TaskId;
-import org.apache.kafka.streams.processor.To;
 import org.junit.After;
 import zipkin2.Span;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 abstract class BaseTracingTest {
   String TEST_APPLICATION_ID = "myAppId";
@@ -33,110 +26,18 @@ abstract class BaseTracingTest {
   String TEST_KEY = "foo";
   String TEST_VALUE = "bar";
 
+
   Function<Headers, ProcessorContext> processorContextSupplier =
       (Headers headers) ->
-          new ProcessorContext() {
 
-            @Override
-            public String applicationId() {
-              return TEST_APPLICATION_ID;
-            }
-
-            @Override
-            public TaskId taskId() {
-              return new TaskId(0, 0);
-            }
-
-            @Override
-            public Serde<?> keySerde() {
-              return null;
-            }
-
-            @Override
-            public Serde<?> valueSerde() {
-              return null;
-            }
-
-            @Override
-            public File stateDir() {
-              return null;
-            }
-
-            @Override
-            public StreamsMetrics metrics() {
-              return null;
-            }
-
-            @Override
-            public void register(StateStore store, StateRestoreCallback stateRestoreCallback) {
-            }
-
-            @Override
-            public StateStore getStateStore(String name) {
-              return null;
-            }
-
-            @Override
-            public Cancellable schedule(long intervalMs, PunctuationType type,
-                Punctuator callback) {
-              return null;
-            }
-
-            @Override
-            public <K, V> void forward(K key, V value) {
-            }
-
-            @Override
-            public <K, V> void forward(K key, V value, To to) {
-            }
-
-            @Override
-            public <K, V> void forward(K key, V value, int childIndex) {
-            }
-
-            @Override
-            public <K, V> void forward(K key, V value, String childName) {
-            }
-
-            @Override
-            public void commit() {
-            }
-
-            @Override
-            public String topic() {
-              return TEST_TOPIC;
-            }
-
-            @Override
-            public int partition() {
-              return 0;
-            }
-
-            @Override
-            public long offset() {
-              return 0;
-            }
-
-            @Override
-            public Headers headers() {
-              return headers;
-            }
-
-            @Override
-            public long timestamp() {
-              return 0;
-            }
-
-            @Override
-            public Map<String, Object> appConfigs() {
-              return null;
-            }
-
-            @Override
-            public Map<String, Object> appConfigsWithPrefix(String prefix) {
-              return null;
-            }
-          };
+      {
+        ProcessorContext processorContext = mock(ProcessorContext.class);
+        when(processorContext.applicationId()).thenReturn(TEST_APPLICATION_ID);
+        when(processorContext.topic()).thenReturn(TEST_TOPIC);
+        when(processorContext.taskId()).thenReturn(new TaskId(0, 0));
+        when(processorContext.headers()).thenReturn(headers);
+        return processorContext;
+      };
 
   ConcurrentLinkedDeque<Span> spans = new ConcurrentLinkedDeque<>();
   Tracing tracing = Tracing.newBuilder()
