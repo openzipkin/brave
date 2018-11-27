fad5ba1eee9bb20d2079006540249e4fd9ed77d9
Adrian Cole <adriancole@users.noreply.github.com>
2018-11-27 10:38:13 +0100

    Update instrumentation/kafka-streams/src/main/java/brave/kafka/streams/KafkaStreamsTracing.java
    Co-Authored-By: jeqo <quilcate.jorge@gmail.com>

--- instrumentation/kafka-streams/src/main/java/brave/kafka/streams/KafkaStreamsTracing.java
+++ instrumentation/kafka-streams/src/main/java/brave/kafka/streams/KafkaStreamsTracing.java
@@ -237,7 +237,7 @@ public final class KafkaStreamsTracing {
 
   /**
    * Create a peek transformer, similar to {@link KStream#mapValues(ValueMapper)}, where its mapper action
-   * will be traced.
+   * will be recorded in a new span with the indicated name.
    *
    * <p>Simple example using Kafka Streams DSL:
    * <pre>{@code
