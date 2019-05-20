df9d8d98357cc4880aebfebeda705e8febe6d7fe
Adrian Cole <acole@pivotal.io>
2019-05-20 20:06:44 +0200

    Converts all the things to ASF build

--- brave/src/main/java/brave/Span.java
+++ brave/src/main/java/brave/Span.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package brave;
 
 import brave.internal.Nullable;
@@ -187,7 +203,7 @@ public abstract class Span implements SpanCustomizer {
    */
   // Design note: This differs from Brave 3's LocalTracer which completes with a given duration.
   // This was changed for a few use cases.
-  // * Finishing a one-way span on another host https://github.com/openzipkin/zipkin/issues/1243
+  // * Finishing a one-way span on another host https://github.com/apache/incubator-zipkin/issues/1243
   //   * The other host will not be able to read the start timestamp, so can't calculate duration
   // * Consistency in Api: All units and measures are epoch microseconds
   //   * This can reduce accidents where people use duration when they mean a timestamp
