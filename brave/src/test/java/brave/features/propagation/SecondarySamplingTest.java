ed77ea03cf0c9f3b365f232c73e8aa41ff770bff
Adrian Cole <adriancole@users.noreply.github.com>
2019-06-03 08:46:31 +0800

    Refines secondary sampling feature test to be realistic (#916)
    @narayuna is one of the first folks to look at this code carefully. I
    think the intent wasn't clear. I'm changing the example to look more
    realistic and also include dynamic configuration hints.

--- brave/src/test/java/brave/features/propagation/SecondarySamplingTest.java
+++ brave/src/test/java/brave/features/propagation/SecondarySamplingTest.java
@@ -20,12 +20,11 @@ import brave.Span;
 import brave.Span.Kind;
 import brave.Tracer;
 import brave.Tracing;
+import brave.features.propagation.SecondarySampling.Extra;
 import brave.handler.FinishedSpanHandler;
 import brave.handler.MutableSpan;
-import brave.propagation.B3SinglePropagation;
 import brave.propagation.TraceContext;
 import brave.propagation.TraceContextOrSamplingFlags;
-import com.google.common.collect.ImmutableMap;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -33,33 +32,33 @@ import java.util.Map;
 import org.junit.After;
 import org.junit.Test;
 
-import static brave.features.propagation.SecondarySampling.Extra;
 import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
+import static java.util.Collections.singletonMap;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SecondarySamplingTest {
   List<zipkin2.Span> zipkin = new ArrayList<>();
-  List<MutableSpan> zeus = new ArrayList<>(), apollo = new ArrayList<>();
-  Map<String, FinishedSpanHandler> stateToFinishedSpanHandler = ImmutableMap.of(
-      "zeus", new FinishedSpanHandler() {
-        @Override public boolean handle(TraceContext context, MutableSpan span) {
-          return zeus.add(span);
-        }
-      },
-      "apollo", new FinishedSpanHandler() {
-        @Override public boolean handle(TraceContext context, MutableSpan span) {
-          return apollo.add(span);
-        }
-      }
-  );
+  List<MutableSpan> edge = new ArrayList<>(), links = new ArrayList<>(), triage =
+    new ArrayList<>();
+  FinishedSpanHandler triageHandler = new FinishedSpanHandler() {
+    @Override public boolean handle(TraceContext context, MutableSpan span) {
+      return triage.add(span);
+    }
+  };
 
-  Tracing tracing = Tracing.newBuilder()
-      .addFinishedSpanHandler(new SecondarySampling.FinishedSpanHandler(stateToFinishedSpanHandler))
-      .propagationFactory(new SecondarySampling.PropagationFactory(
-          B3SinglePropagation.FACTORY, stateToFinishedSpanHandler.keySet()
-      ))
-      .spanReporter(zipkin::add)
-      .build();
+  SecondarySampling secondarySampling = SecondarySampling.create()
+    .putSystem("edge", new FinishedSpanHandler() {
+      @Override public boolean handle(TraceContext context, MutableSpan span) {
+        return edge.add(span);
+      }
+    })
+    .putSystem("links", new FinishedSpanHandler() {
+      @Override public boolean handle(TraceContext context, MutableSpan span) {
+        return links.add(span);
+      }
+    });
+  Tracing tracing = secondarySampling.build(Tracing.newBuilder().spanReporter(zipkin::add));
 
   TraceContext.Extractor<Map<String, String>> extractor = tracing.propagation().extractor(Map::get);
   TraceContext.Injector<Map<String, String>> injector = tracing.propagation().injector(Map::put);
@@ -70,106 +69,142 @@ public class SecondarySamplingTest {
     tracing.close();
   }
 
-  /** This shows when primary trace status is not sampled, we can send to handlers anyway. */
+  /**
+   * This shows when primary trace status is not sampled, we can send to handlers anyway.
+   *
+   * <p>At first, "triage" is not configured, so the tracer ignores it. Later, it is configured, so
+   * starts receiving traces.
+   */
   @Test public void integrationTest() {
     map.put("b3", "0");
-    map.put("sampling", "zeus:rate=1.0,ttl=3;apollo:sampled=1;wookie:rate=0.05");
+    map.put("sampling", "edge:tps=1,ttl=3;links:sampled=1;triage:tps=5");
 
     Tracer tracer = tracing.tracer();
     Span span1 = tracer.nextSpan(extractor.extract(map)).name("span1").kind(Kind.SERVER).start();
     Span span2 = tracer.newChild(span1.context()).kind(Kind.CLIENT).name("span2").start();
     injector.inject(span2.context(), map);
-    assertThat(map).containsEntry("sampling",
-        "zeus:ttl=3,sampled=1;apollo:sampled=1;wookie:rate=0.05");
+    assertThat(map).containsEntry("sampling", "edge:ttl=3,sampled=1;links:sampled=1;triage:tps=5");
 
     // hop 1
-    Span span3 = tracer.joinSpan(extractor.extract(map).context()).kind(Kind.SERVER).start();
+    Span span3 = tracer.nextSpan(extractor.extract(map)).kind(Kind.SERVER).start();
     Span span4 = tracer.newChild(span3.context()).kind(Kind.CLIENT).name("span3").start();
     injector.inject(span4.context(), map);
-    assertThat(map).containsEntry("sampling",
-        "zeus:sampled=1,ttl=2;apollo:sampled=1;wookie:rate=0.05");
+    assertThat(map).containsEntry("sampling", "edge:sampled=1,ttl=2;links:sampled=1;triage:tps=5");
 
     // hop 2
-    Span span5 = tracer.joinSpan(extractor.extract(map).context()).kind(Kind.SERVER).start();
+    Span span5 = tracer.nextSpan(extractor.extract(map)).kind(Kind.SERVER).start();
     Span span6 = tracer.newChild(span5.context()).kind(Kind.CLIENT).name("span4").start();
     injector.inject(span6.context(), map);
-    assertThat(map).containsEntry("sampling",
-        "zeus:sampled=1,ttl=1;apollo:sampled=1;wookie:rate=0.05");
+    assertThat(map).containsEntry("sampling", "edge:sampled=1,ttl=1;links:sampled=1;triage:tps=5");
 
     // hop 3
-    Span span7 = tracer.joinSpan(extractor.extract(map).context()).kind(Kind.SERVER).start();
+    Span span7 = tracer.nextSpan(extractor.extract(map)).kind(Kind.SERVER).start();
     Span span8 = tracer.newChild(span7.context()).kind(Kind.CLIENT).name("span5").start();
     injector.inject(span8.context(), map);
-    assertThat(map).containsEntry("sampling", "apollo:sampled=1;wookie:rate=0.05");
+    assertThat(map).containsEntry("sampling", "links:sampled=1;triage:tps=5");
 
-    // hop 4
-    Span span9 = tracer.joinSpan(extractor.extract(map).context()).kind(Kind.SERVER).start();
+    // dynamic configuration adds triage processing
+    secondarySampling.putSystem("triage", triageHandler);
+
+    // hop 4, triage is now sampled
+    Span span9 = tracer.nextSpan(extractor.extract(map)).kind(Kind.SERVER).start();
     Span span10 = tracer.newChild(span9.context()).kind(Kind.CLIENT).name("span6").start();
     injector.inject(span10.context(), map);
-    assertThat(map).containsEntry("sampling", "apollo:sampled=1;wookie:rate=0.05");
+    assertThat(map).containsEntry("sampling", "links:sampled=1;triage:sampled=1");
 
     asList(span1, span2, span3, span4, span5, span6, span7, span8, span9, span10)
-        .forEach(Span::finish);
+      .forEach(Span::finish);
 
     assertThat(zipkin).isEmpty();
-    assertThat(zeus).filteredOn(s -> s.kind() == Kind.SERVER).hasSize(4);
-    assertThat(apollo).filteredOn(s -> s.kind() == Kind.SERVER).hasSize(5);
+    assertThat(edge).filteredOn(s -> s.kind() == Kind.SERVER).hasSize(3);
+    assertThat(links).filteredOn(s -> s.kind() == Kind.SERVER).hasSize(5);
+    assertThat(triage).filteredOn(s -> s.kind() == Kind.SERVER).hasSize(1);
   }
 
   @Test public void extract_samplesLocalWhenConfigured() {
     map.put("b3", "0");
-    map.put("sampling", "apollo:sampled=0;wookie:rate=0.05");
+    map.put("sampling", "links:sampled=0;triage:tps=5");
 
     assertThat(extractor.extract(map).sampledLocal()).isFalse();
 
     map.put("b3", "0");
-    map.put("sampling", "apollo:sampled=0;wookie:sampled=1");
+    map.put("sampling", "links:sampled=0;triage:sampled=1");
 
     assertThat(extractor.extract(map).sampledLocal()).isFalse();
 
     map.put("b3", "0");
-    map.put("sampling", "apollo:sampled=1;wookie:rate=0.05");
+    map.put("sampling", "links:sampled=1;triage:tps=5");
 
     assertThat(extractor.extract(map).sampledLocal()).isTrue();
   }
 
-  @Test public void extract_convertsConfiguredRateToDecision() {
+  /** This shows an example of dynamic configuration */
+  @Test public void dynamicConfiguration() {
+    // base case: links is configured, triage is not. triage is in the headers, though!
     map.put("b3", "0");
-    map.put("sampling", "zeus:rate=1.0,ttl=3;apollo:sampled=0;wookie:rate=0.05");
+    map.put("sampling", "links:sampled=1;triage:tps=5");
+
+    assertThat(extractor.extract(map).sampledLocal()).isTrue();
+
+    // dynamic configuration removes link processing
+    secondarySampling.removeSystem("links");
+    assertThat(extractor.extract(map).sampledLocal()).isFalse();
+
+    // dynamic configuration adds triage processing
+    secondarySampling.putSystem("triage", triageHandler);
+    assertThat(extractor.extract(map).sampledLocal()).isTrue();
+
+    tracing.tracer().nextSpan(extractor.extract(map)).start().finish();
+    assertThat(zipkin).isEmpty();
+    assertThat(edge).isEmpty();
+    assertThat(links).isEmpty(); // no longer configured
+    assertThat(triage).hasSize(1); // now configured
+  }
+
+  @Test public void extract_convertsConfiguredTpsToDecision() {
+    map.put("b3", "0");
+    map.put("sampling", "edge:tps=1,ttl=3;links:sampled=0;triage:tps=5");
 
     TraceContextOrSamplingFlags extracted = extractor.extract(map);
     Extra extra = (Extra) extracted.extra().get(0);
-    assertThat(extra.states)
-        .containsEntry("zeus", ImmutableMap.of("sampled", "1", "ttl", "3"))
-        .containsEntry("apollo", ImmutableMap.of("sampled", "0"))
-        .containsEntry("wookie", ImmutableMap.of("rate", "0.05"));
+    assertThat(extra.systems)
+      .containsEntry("edge", twoEntryMap("sampled", "1", "ttl", "3"))
+      .containsEntry("links", singletonMap("sampled", "0"))
+      .containsEntry("triage", singletonMap("tps", "5")); // triage is not configured
   }
 
   @Test public void extract_decrementsTtlWhenConfigured() {
     map.put("b3", "0");
-    map.put("sampling", "zeus:sampled=1,ttl=3;apollo:sampled=0,ttl=1;wookie:rate=0.05");
+    map.put("sampling", "edge:sampled=1,ttl=3;links:sampled=0,ttl=1;triage:tps=5");
 
     TraceContextOrSamplingFlags extracted = extractor.extract(map);
     Extra extra = (Extra) extracted.extra().get(0);
-    assertThat(extra.states)
-        .containsEntry("zeus", ImmutableMap.of("sampled", "1", "ttl", "2"))
-        .doesNotContainKey("apollo")
-        .containsEntry("wookie", ImmutableMap.of("rate", "0.05"));
+    assertThat(extra.systems)
+      .containsEntry("edge", twoEntryMap("sampled", "1", "ttl", "2"))
+      .doesNotContainKey("links")
+      .containsEntry("triage", singletonMap("tps", "5"));
   }
 
-  @Test public void injectWritesAllStates() {
+  @Test public void injectWritesAllSystems() {
     Extra extra = new Extra();
-    extra.states.put("zeus", ImmutableMap.of("rate", "1.0", "ttl", "3"));
-    extra.states.put("apollo", ImmutableMap.of("sampled", "0"));
-    extra.states.put("wookie", ImmutableMap.of("rate", "0.05"));
+    extra.systems.put("edge", twoEntryMap("tps", "1", "ttl", "3"));
+    extra.systems.put("links", singletonMap("sampled", "0"));
+    extra.systems.put("triage", singletonMap("tps", "5"));
 
     injector.inject(TraceContext.newBuilder()
-        .traceId(1L).spanId(2L)
-        .sampled(false)
-        .extra(asList(extra))
-        .build(), map);
+      .traceId(1L).spanId(2L)
+      .sampled(false)
+      .extra(singletonList(extra))
+      .build(), map);
 
     assertThat(map)
-        .containsEntry("sampling", "zeus:rate=1.0,ttl=3;apollo:sampled=0;wookie:rate=0.05");
+      .containsEntry("sampling", "edge:tps=1,ttl=3;links:sampled=0;triage:tps=5");
+  }
+
+  static <K, V> Map<K, V> twoEntryMap(K key1, V value1, K key2, V value2) {
+    Map<K, V> result = new LinkedHashMap<>();
+    result.put(key1, value1);
+    result.put(key2, value2);
+    return result;
   }
 }
