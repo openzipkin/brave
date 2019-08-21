3a71caf3feebb197ec55978e492314ffa3b683de
Adrian Cole <adriancole@users.noreply.github.com>
2019-08-21 18:58:37 +0800

    Refines test to show only one use-case fails with ActiveMQ BytesMessage (#968)
    MessageListener doesn't fail. What fails is receive. We want `receive()`
    to return a message which includes a "b3" header for later processing.
    When ActiveMQ 5.x BytesMessage, this fails due to a guard on writing a
    message when in read-only mode.
    
    See #967

--- instrumentation/jms/src/test/java/brave/jms/ITJms_2_0_TracingMessageConsumer.java
+++ instrumentation/jms/src/test/java/brave/jms/ITJms_2_0_TracingMessageConsumer.java
@@ -13,11 +13,17 @@
  */
 package brave.jms;
 
+import org.junit.Test;
 import org.junit.rules.TestName;
 
 /** When adding tests here, also add to {@linkplain brave.jms.ITTracingJMSConsumer} */
 public class ITJms_2_0_TracingMessageConsumer extends ITJms_1_1_TracingMessageConsumer {
   @Override JmsTestRule newJmsTestRule(TestName testName) {
     return new ArtemisJmsTestRule(testName);
+  }
+
+  // Inability to encode "b3" on a received BytesMessage only applies to ActiveMQ 5.x
+  @Test public void receive_resumesTrace_bytes() throws Exception {
+    receive_resumesTrace(() -> messageProducer.send(bytesMessage), messageConsumer);
   }
 }
