31b6de198c798e6efd626e2ccee12b81f7f8c269
Adrian Cole <acole@pivotal.io>
2019-10-30 16:55:08 +0800

    instanceof

--- brave/src/main/java/brave/Request.java
+++ brave/src/main/java/brave/Request.java
@@ -40,8 +40,8 @@ public abstract class Request {
    *
    * <p>Note: Some implementations are composed of multiple types, such as a request and a socket
    * address of the client. Moreover, an implementation may change the type returned due to
-   * refactoring. Unless you control the implementation, cast carefully (ex using {@code instance
-   * of}) instead of presuming a specific type will always be returned.
+   * refactoring. Unless you control the implementation, cast carefully (ex using {@code
+   * instanceof}) instead of presuming a specific type will always be returned.
    *
    * @since 5.9
    */
