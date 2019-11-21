416675de5426e664fb0ca941bf5294ba166a7716
Matúš Hennel <me3x@users.noreply.github.com>
2019-11-21 15:15:08 +0100

    fix re-setting properties on BytesMessage
    fix MessageNotWriteableException Message body is read-only while re-setting properties on BytesMessage

--- instrumentation/jms/src/main/java/brave/jms/PropertyFilter.java
+++ instrumentation/jms/src/main/java/brave/jms/PropertyFilter.java
@@ -13,88 +13,112 @@
  */
 package brave.jms;
 
+import static brave.jms.JmsTracing.log;
+import javax.jms.BytesMessage;
+import javax.jms.JMSException;
+import javax.jms.Message;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
 import java.util.Set;
-import javax.jms.Message;
-
-import static brave.internal.Throwables.propagateIfFatal;
-import static brave.jms.JmsTracing.log;
+import brave.internal.Platform;
 
 // Similar to https://github.com/apache/camel/blob/b9a3117f19dd19abd2ea8b789c42c3e86fe4c488/components/camel-jms/src/main/java/org/apache/camel/component/jms/JmsMessageHelper.java
 final class PropertyFilter {
-  /**
-   * This implies copying properties because the JMS spec says you can't write properties upon
-   * receipt until {@link Message#clearProperties()} has been called.
-   *
-   * <p> See https://docs.oracle.com/javaee/6/api/javax/jms/Message.html
-   */
-  static void filterProperties(Message message, Set<String> namesToClear) {
-    ArrayList<Object> retainedProperties = messagePropertiesBuffer();
-    try {
-      filterProperties(message, namesToClear, retainedProperties);
-    } finally {
-      retainedProperties.clear(); // ensure no object references are held due to any exception
-    }
-  }
 
-  static void filterProperties(Message message, Set<String> namesToClear, List<Object> out) {
-    Enumeration<?> names;
-    try {
-      names = message.getPropertyNames();
-    } catch (Throwable t) {
-      propagateIfFatal(t);
-      log(t, "error getting property names from {0}", message, null);
-      return;
+    /**
+     * This implies copying properties because the JMS spec says you can't write properties upon
+     * receipt until {@link Message#clearProperties()} has been called.
+     * <p> See https://docs.oracle.com/javaee/6/api/javax/jms/Message.html
+     */
+    static void filterProperties( Message message, Set<String> namesToClear ) {
+        ArrayList<Object> retainedProperties = messagePropertiesBuffer();
+        try {
+            filterProperties( message, namesToClear, retainedProperties );
+        } finally {
+            retainedProperties.clear(); // ensure no object references are held due to any exception
+        }
     }
 
-    while (names.hasMoreElements()) {
-      String name = (String) names.nextElement();
-      Object value;
-      try {
-        value = message.getObjectProperty(name);
-      } catch (Throwable t) {
-        propagateIfFatal(t);
-        log(t, "error getting property {0} from message {1}", name, message);
-        return;
-      }
-      if (!namesToClear.contains(name) && value != null) {
-        out.add(name);
-        out.add(value);
-      }
+    static void filterProperties( Message message, Set<String> namesToClear, List<Object> out ) {
+        Enumeration<?> names;
+        try {
+            names = message.getPropertyNames();
+        } catch ( JMSException e ) {
+            Platform.get().log( "error getting property names from {0}", message, e );
+            return;
+        }
+
+        while ( names.hasMoreElements() ) {
+            String name = (String) names.nextElement();
+            Object value;
+            try {
+                value = message.getObjectProperty( name );
+            } catch ( JMSException e ) {
+                log( e, "error getting property {0} from message {1}", name, message );
+                return;
+            }
+            if ( !namesToClear.contains( name ) && value != null ) {
+                out.add( name );
+                out.add( value );
+            }
+        }
+
+        // redo the properties to keep
+        try {
+            message.clearProperties();
+        } catch ( JMSException e ) {
+            Platform.get().log( "error clearing properties of {0}", message, e );
+            return;
+        }
+
+        // workaround for ActiveMQBytesMessage
+        if ( message instanceof BytesMessage ) {
+            resetBytesMessageProperties( message, out );
+        } else {
+            resetProperties( message, out );
+        }
+
     }
 
-    // redo the properties to keep
-    try {
-      message.clearProperties();
-    } catch (Throwable t) {
-      propagateIfFatal(t);
-      log(t, "error clearing properties of {0}", message, null);
-      return;
+    private static void resetBytesMessageProperties( Message message, List<Object> out ) {
+        try {
+            BytesMessage bytesMessage = (BytesMessage) message;
+            byte[] body = new byte[(int) bytesMessage.getBodyLength()];
+            bytesMessage.reset();
+            bytesMessage.readBytes( body );
+            message.clearBody();
+            resetProperties( message, out );
+            bytesMessage.writeBytes( body );
+            bytesMessage.reset();
+        } catch ( JMSException e ) {
+            log( e, "unable to reset BytesMessage body {0}", message, e );
+        }
     }
 
-    for (int i = 0, length = out.size(); i < length; i += 2) {
-      String name = out.get(i).toString();
-      try {
-        message.setObjectProperty(name, out.get(i + 1));
-      } catch (Throwable t) {
-        propagateIfFatal(t);
-        log(t, "error setting property {0} on message {1}", name, message);
-        // continue on error when re-setting properties as it is better than not.
-      }
+    private static void resetProperties( Message message, List<Object> out ) {
+        for ( int i = 0, length = out.size(); i < length; i += 2 ) {
+            String name = out.get( i ).toString();
+            try {
+                message.setObjectProperty( name, out.get( i + 1 ) );
+            } catch ( JMSException e ) {
+                log( e, "error setting property {0} on message {1}", name, message );
+                // continue on error when re-setting properties as it is better than not.
+            }
+        }
     }
-  }
 
-  static final ThreadLocal<ArrayList<Object>> MESSAGE_PROPERTIES_BUFFER = new ThreadLocal<>();
+    static final ThreadLocal<ArrayList<Object>> MESSAGE_PROPERTIES_BUFFER = new ThreadLocal<>();
 
-  /** Also use pair indexing for temporary message properties: (name, value). */
-  static ArrayList<Object> messagePropertiesBuffer() {
-    ArrayList<Object> messagePropertiesBuffer = MESSAGE_PROPERTIES_BUFFER.get();
-    if (messagePropertiesBuffer == null) {
-      messagePropertiesBuffer = new ArrayList<>();
-      MESSAGE_PROPERTIES_BUFFER.set(messagePropertiesBuffer);
+    /**
+     * Also use pair indexing for temporary message properties: (name, value).
+     */
+    static ArrayList<Object> messagePropertiesBuffer() {
+        ArrayList<Object> messagePropertiesBuffer = MESSAGE_PROPERTIES_BUFFER.get();
+        if ( messagePropertiesBuffer == null ) {
+            messagePropertiesBuffer = new ArrayList<>();
+            MESSAGE_PROPERTIES_BUFFER.set( messagePropertiesBuffer );
+        }
+        return messagePropertiesBuffer;
     }
-    return messagePropertiesBuffer;
-  }
 }
