bcd58a556c8a0abfdb17679ea861a3a7f722acb5
Adrian Cole <acole@pivotal.io>
2020-02-24 16:20:20 +0800

    Adds Spring Beans for HttpRequestParser and HttpResponseParser
    This is Spring integration for #1084

--- spring-beans/src/main/java/brave/spring/beans/HttpTracingFactoryBean.java
+++ spring-beans/src/main/java/brave/spring/beans/HttpTracingFactoryBean.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2019 The OpenZipkin Authors
+ * Copyright 2013-2020 The OpenZipkin Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -16,6 +16,8 @@ package brave.spring.beans;
 import brave.Tracing;
 import brave.http.HttpClientParser;
 import brave.http.HttpRequest;
+import brave.http.HttpRequestParser;
+import brave.http.HttpResponseParser;
 import brave.http.HttpServerParser;
 import brave.http.HttpTracing;
 import brave.http.HttpTracingCustomizer;
@@ -29,12 +31,18 @@ public class HttpTracingFactoryBean implements FactoryBean {
   Tracing tracing;
   HttpClientParser clientParser;
   HttpServerParser serverParser;
+  HttpRequestParser clientRequestParser, serverRequestParser;
+  HttpResponseParser clientResponseParser, serverResponseParser;
   SamplerFunction<HttpRequest> clientSampler, serverSampler;
   List<HttpTracingCustomizer> customizers;
 
   @Override public HttpTracing getObject() {
     HttpTracing.Builder builder = HttpTracing.newBuilder(tracing);
     if (clientParser != null) builder.clientParser(clientParser);
+    if (clientRequestParser != null) builder.clientRequestParser(clientRequestParser);
+    if (clientResponseParser != null) builder.clientResponseParser(clientResponseParser);
+    if (serverRequestParser != null) builder.serverRequestParser(serverRequestParser);
+    if (serverResponseParser != null) builder.serverResponseParser(serverResponseParser);
     if (serverParser != null) builder.serverParser(serverParser);
     if (clientSampler != null) builder.clientSampler(clientSampler);
     if (serverSampler != null) builder.serverSampler(serverSampler);
@@ -58,6 +66,22 @@ public class HttpTracingFactoryBean implements FactoryBean {
 
   public void setClientParser(HttpClientParser clientParser) {
     this.clientParser = clientParser;
+  }
+
+  public void setClientRequestParser(HttpRequestParser clientRequestParser) {
+    this.clientRequestParser = clientRequestParser;
+  }
+
+  public void setClientResponseParser(HttpResponseParser clientResponseParser) {
+    this.clientResponseParser = clientResponseParser;
+  }
+
+  public void setServerRequestParser(HttpRequestParser serverRequestParser) {
+    this.serverRequestParser = serverRequestParser;
+  }
+
+  public void setServerResponseParser(HttpResponseParser serverResponseParser) {
+    this.serverResponseParser = serverResponseParser;
   }
 
   public void setServerParser(HttpServerParser serverParser) {
