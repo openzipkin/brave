d4a0f1d43a26363ad372071b39e029078c1bdc51
Csaba Kos <csaba@indeed.com>
2020-02-27 09:37:55 -0600

    Small performance and documentation tweaks. Fixed version number in POM.

--- instrumentation/mongodb/src/main/java/brave/mongodb/MongoDBTracing.java
+++ instrumentation/mongodb/src/main/java/brave/mongodb/MongoDBTracing.java
@@ -20,6 +20,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -46,24 +47,25 @@ public final class MongoDBTracing {
     return new TraceMongoCommandListener(this);
   }
 
-  public final static class Builder {
+  public static final class Builder {
     final Tracing tracing;
     // See https://docs.mongodb.com/manual/reference/command for the command reference
-    final Set<String> commandsWithCollectionName = new HashSet<>(Arrays.asList("aggregate", "count", "distinct",
+    static final List<String> DEFAULT_COMMANDS_WITH_COLLECTION_NAME = Arrays.asList("aggregate", "count", "distinct",
       "mapReduce", "geoSearch", "delete", "find", "findAndModify", "insert", "update", "collMod", "compact",
-      "convertToCapped", "create", "createIndexes", "drop", "dropIndexes", "killCursors", "listIndexes", "reIndex"));
+      "convertToCapped", "create", "createIndexes", "drop", "dropIndexes", "killCursors", "listIndexes", "reIndex");
+    final Set<String> commandsWithCollectionName = new HashSet<>();
     int maxAbbreviatedCommandLength = 1000;
 
     Builder(Tracing tracing) {
       if (tracing == null) throw new NullPointerException("tracing == null");
       this.tracing = tracing;
+      commandsWithCollectionName.addAll(DEFAULT_COMMANDS_WITH_COLLECTION_NAME);
     }
 
     Builder(MongoDBTracing mongoDBTracing) {
-      this(mongoDBTracing.tracing);
-      maxAbbreviatedCommandLength(mongoDBTracing.maxAbbreviatedCommandLength);
-      clearCommandsWithCollectionName();
-      addAllCommandsWithCollectionName(mongoDBTracing.commandsWithCollectionName);
+      tracing = mongoDBTracing.tracing;
+      maxAbbreviatedCommandLength = mongoDBTracing.maxAbbreviatedCommandLength;
+      commandsWithCollectionName.addAll(mongoDBTracing.commandsWithCollectionName);
     }
 
     /**
@@ -93,8 +95,10 @@ public final class MongoDBTracing {
      * argument contains the collection/view name. Tracing will only attempt to extract the collection/view name
      * argument from allow-listed commands.
      *
-     * The default allow-list is a set of commonly used commands ({@link #commandsWithCollectionName}) that operate on
-     * collections/views.
+     * The default allow-list is a set of commonly used commands ({@link #DEFAULT_COMMANDS_WITH_COLLECTION_NAME}) that
+     * operate on collections/views.
+     *
+     * To replace the list completely, don't forget to call {@link #clearCommandsWithCollectionName()} first.
      *
      * @param commandName command name to add to the allow-list
      */
