2f8aecc52ee1d8135a31319c73a842d37b1b3b7e
Adrian Cole <acole@pivotal.io>
2020-03-17 11:39:01 +0800

    adds classloader test

---
+++ propagation/w3c/src/test/java/brave/propagation/w3c/TraceContextPropagationClassLoaderTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2013-2020 The OpenZipkin Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package brave.propagation.w3c;
+
+import brave.propagation.Propagation;
+import brave.propagation.Propagation.KeyFactory;
+import brave.propagation.TraceContext;
+import brave.propagation.TraceContext.Extractor;
+import brave.propagation.TraceContext.Injector;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import org.junit.Test;
+
+import static brave.test.util.ClassLoaders.assertRunIsUnloadable;
+
+public class TraceContextPropagationClassLoaderTest {
+  @Test public void unloadable_afterBasicUsage() {
+    assertRunIsUnloadable(BasicUsage.class, getClass().getClassLoader());
+  }
+
+  static class BasicUsage implements Runnable {
+    @Override public void run() {
+      Propagation<String> propagation = TraceContextPropagation.FACTORY.create(KeyFactory.STRING);
+      Injector<Map<String, String>> injector = propagation.injector(Map::put);
+      Extractor<Map<String, String>> extractor = propagation.extractor(Map::get);
+
+      TraceContext context = TraceContext.newBuilder().traceId(1L).spanId(2L).build();
+
+      Map<String, String> headers = new LinkedHashMap<>();
+      injector.inject(context, headers);
+
+      String traceparent = headers.get("traceparent");
+      if (!"00-00000000000000000000000000000001-0000000000000002-00".equals(traceparent)) {
+        throw new AssertionError();
+      }
+
+      if (!context.equals(extractor.extract(headers).context())) {
+        throw new AssertionError();
+      }
+    }
+  }
+}
