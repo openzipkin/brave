435b7cac5c2beea0d680357b8444223bb0ac33db
Adrian Cole <acole@pivotal.io>
2020-03-18 18:26:32 +0800

    better perf and better tests

--- propagation/w3c/src/test/java/brave/propagation/w3c/TracestateFormatTest.java
+++ propagation/w3c/src/test/java/brave/propagation/w3c/TracestateFormatTest.java
@@ -34,22 +34,18 @@ public class TracestateFormatTest {
     assertThat(TracestateFormat.validateKey("")).isFalse();
   }
 
-  @Test public void validateKey_unicode() {
-    String key = "a_💩_day";
-    assertThat(TracestateFormat.validateKey(key)).isFalse();
-  }
-
   @Test public void validateKey_tooLong() {
     char[] tooMany = new char[257];
     assertThat(TracestateFormat.validateKey(new String(tooMany))).isFalse();
   }
 
   @Test public void validateKey_shortest_basic() {
-    assertThat(TracestateFormat.validateKey("a")).isTrue();
+    assertThat(TracestateFormat.validateKey("z")).isTrue();
   }
 
   @Test public void validateKey_shortest_tenant() {
-    assertThat(TracestateFormat.validateKey("1@a")).isTrue();
+    assertThat(TracestateFormat.validateKey("0@z")).isTrue();
+    assertThat(TracestateFormat.validateKey("a@z")).isTrue();
   }
 
   @Test public void validateKey_longest_basic() {
@@ -58,5 +54,39 @@ public class TracestateFormatTest {
 
   @Test public void validateKey_longest_tenant() {
     assertThat(TracestateFormat.validateKey(LONGEST_TENANT_KEY)).isTrue();
+  }
+
+  @Test public void validateKey_invalid_basic() {
+    // zero is allowed only as when there is an '@'
+    assertThat(TracestateFormat.validateKey("0")).isFalse();
+  }
+
+  @Test public void validateKey_invalid_basic_unicode() {
+    assertThat(TracestateFormat.validateKey("a💩")).isFalse();
+    assertThat(TracestateFormat.validateKey("💩a")).isFalse();
+  }
+
+  @Test public void validateKey_invalid_tenant() {
+    assertThat(TracestateFormat.validateKey("_@z")).isFalse();
+  }
+
+  @Test public void validateKey_invalid_tenant_unicode() {
+    assertThat(TracestateFormat.validateKey("a@a💩")).isFalse();
+    assertThat(TracestateFormat.validateKey("a@💩a")).isFalse();
+    assertThat(TracestateFormat.validateKey("a💩@a")).isFalse();
+    assertThat(TracestateFormat.validateKey("💩a@a")).isFalse();
+  }
+
+  @Test public void validateKey_invalid_tenant_empty() {
+    assertThat(TracestateFormat.validateKey("@a")).isFalse();
+    assertThat(TracestateFormat.validateKey("a@")).isFalse();
+  }
+
+  @Test public void validateKey_invalid_tenant_vendor_longest() {
+    assertThat(TracestateFormat.validateKey("a@abcdef12345678")).isTrue();
+  }
+
+  @Test public void validateKey_invalid_tenant_vendor_tooLong() {
+    assertThat(TracestateFormat.validateKey("a@abcdef1234567890")).isFalse();
   }
 }
