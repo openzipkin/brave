8920d3c89d0c4243f9626241eb5dd62a5cc93d51
Adrian Cole <acole@pivotal.io>
2020-04-27 08:32:54 +0800

    Renovates gRPC instrumentation in preparation of standard parsing
    This fixes some glitches in preparation of #999
    
    * Bumps gRPC to 1.29 (there was no impact)
    * GrpcClientRequest and GrpcServerRequest unwrapped `this`, not the call.
    * Anonymous GrpcClientParser and GrpcServerParser used ErrorParser unnecessarily
      * This is implicitly done in the Zipkin span handler
    * Status.cause was ignored
      * This led to a less precise "error" tag
    * Client headers were unnecessarily null during sampling. This was caused by
      too eager allocation of a span.
    * ClientCall.Listener was wrapped twice
    * Most ClientCall hooks were not placed in scope
    * ServerCall's span was started too late

--- instrumentation/grpc/src/main/java/brave/grpc/TracingClientInterceptor.java
+++ instrumentation/grpc/src/main/java/brave/grpc/TracingClientInterceptor.java
@@ -13,7 +13,9 @@
  */
 package brave.grpc;
 
+import brave.NoopSpanCustomizer;
 import brave.Span;
+import brave.SpanCustomizer;
 import brave.Tracer;
 import brave.internal.Nullable;
 import brave.propagation.CurrentTraceContext;
@@ -30,95 +32,165 @@ import io.grpc.ClientInterceptor;
 import io.grpc.ForwardingClientCall.SimpleForwardingClientCall;
 import io.grpc.ForwardingClientCallListener.SimpleForwardingClientCallListener;
 import io.grpc.Metadata;
+import io.grpc.Metadata.Key;
 import io.grpc.MethodDescriptor;
 import io.grpc.Status;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
 
 import static brave.grpc.GrpcClientRequest.SETTER;
 
 // not exposed directly as implementation notably changes between versions 1.2 and 1.3
 final class TracingClientInterceptor implements ClientInterceptor {
-  final Tracer tracer;
+  final Map<String, Key<String>> nameToKey;
   final CurrentTraceContext currentTraceContext;
+  final Tracer tracer;
   final SamplerFunction<RpcRequest> sampler;
   final Injector<GrpcClientRequest> injector;
   final GrpcClientParser parser;
-  final Map<String, Metadata.Key<String>> nameToKey;
+  final MessageProcessor messageProcessor;
 
   TracingClientInterceptor(GrpcTracing grpcTracing) {
-    tracer = grpcTracing.rpcTracing.tracing().tracer();
+    nameToKey = grpcTracing.nameToKey;
     currentTraceContext = grpcTracing.rpcTracing.tracing().currentTraceContext();
+    tracer = grpcTracing.rpcTracing.tracing().tracer();
     sampler = grpcTracing.rpcTracing.clientSampler();
     injector = grpcTracing.propagation.injector(SETTER);
     parser = grpcTracing.clientParser;
-    nameToKey = grpcTracing.nameToKey;
+    messageProcessor = grpcTracing.clientMessageProcessor;
   }
 
   @Override
   public <ReqT, RespT> ClientCall<ReqT, RespT> interceptCall(MethodDescriptor<ReqT, RespT> method,
     CallOptions callOptions, Channel next) {
-    TraceContext invocationContext = currentTraceContext.get();
-
-    GrpcClientRequest request = new GrpcClientRequest(nameToKey, method);
-    Span span = tracer.nextSpanWithParent(sampler, request, invocationContext);
-
-    Throwable error = null;
-    try (Scope scope = currentTraceContext.maybeScope(span.context())) {
-      return new SimpleForwardingClientCall<ReqT, RespT>(next.newCall(method, callOptions)) {
-        @Override public void start(Listener<RespT> responseListener, Metadata headers) {
-          request.metadata = headers;
-          injector.inject(span.context(), request);
-          span.kind(Span.Kind.CLIENT).start();
-          try (Scope scope = currentTraceContext.maybeScope(span.context())) {
-            parser.onStart(method, callOptions, headers, span.customizer());
-
-            // See RATIONALE.md which notes response callbacks are in the invocation context
-            responseListener = new TraceContextCallListener<>(
-              responseListener,
-              currentTraceContext,
-              invocationContext
-            );
-
-            super.start(new TracingClientCallListener<>(responseListener, span), headers);
-          }
-        }
-
-        @Override public void sendMessage(ReqT message) {
-          try (Scope scope = currentTraceContext.maybeScope(span.context())) {
-            super.sendMessage(message);
-            parser.onMessageSent(message, span.customizer());
-          }
-        }
-      };
-    } catch (Throwable e) {
-      error = e;
-      throw e;
-    } finally {
-      if (error != null) span.error(error).finish();
-    }
+    return new TracingClientCall<>(
+      method, callOptions, currentTraceContext.get(), next.newCall(method, callOptions));
   }
 
-  static final class TraceContextCallListener<RespT>
-    extends SimpleForwardingClientCallListener<RespT> {
-    final CurrentTraceContext currentTraceContext;
-    @Nullable final TraceContext invocationContext;
+  void finish(GrpcClientResponse response, @Nullable Span span) {
+    if (span == null || span.isNoop()) return;
+    Throwable error = response.error();
+    if (error != null) span.error(error);
+    parser.onClose(response.status, response.trailers, span.customizer());
+    span.finish();
+  }
 
-    TraceContextCallListener(
-      Listener<RespT> delegate,
-      CurrentTraceContext currentTraceContext,
-      @Nullable TraceContext invocationContext
-    ) {
-      super(delegate);
-      this.currentTraceContext = currentTraceContext;
+  void finishWithError(@Nullable Span span, Throwable error) {
+    if (span == null || span.isNoop()) return;
+    if (error != null) span.error(error);
+    span.finish();
+  }
+
+  final class TracingClientCall<ReqT, RespT> extends SimpleForwardingClientCall<ReqT, RespT> {
+    final MethodDescriptor<ReqT, RespT> method;
+    final CallOptions callOptions;
+    final TraceContext invocationContext;
+    final AtomicReference<Span> spanRef = new AtomicReference<>();
+
+    TracingClientCall(MethodDescriptor<ReqT, RespT> method, CallOptions callOptions,
+      TraceContext invocationContext, ClientCall<ReqT, RespT> call) {
+      super(call);
+      this.method = method;
+      this.callOptions = callOptions;
       this.invocationContext = invocationContext;
     }
 
+    @Override public void start(Listener<RespT> responseListener, Metadata headers) {
+      GrpcClientRequest request =
+        new GrpcClientRequest(nameToKey, method, callOptions, delegate(), headers);
+
+      Span span = tracer.nextSpanWithParent(sampler, request, invocationContext);
+      injector.inject(span.context(), request);
+      if (!span.isNoop()) {
+        span.kind(Span.Kind.CLIENT).start();
+        parser.onStart(method, callOptions, headers, span.customizer());
+      }
+      spanRef.set(span);
+
+      responseListener = new TracingClientCallListener<>(
+        responseListener,
+        invocationContext,
+        spanRef,
+        request
+      );
+
+      try (Scope scope = currentTraceContext.maybeScope(span.context())) {
+        super.start(responseListener, headers);
+      } catch (Throwable e) {
+        // Another interceptor may throw an exception during start, in which case no other
+        // callbacks are called, so go ahead and close the span here.
+        finishWithError(spanRef.getAndSet(null), e);
+        throw e;
+      }
+    }
+
+    @Override public void cancel(@Nullable String message, @Nullable Throwable cause) {
+      try (Scope scope = maybeScopeClientOrInvocationContext(spanRef, invocationContext)) {
+        delegate().cancel(message, cause);
+      }
+    }
+
+    @Override public void halfClose() {
+      try (Scope scope = maybeScopeClientOrInvocationContext(spanRef, invocationContext)) {
+        delegate().halfClose();
+      } catch (Throwable e) {
+        // If there was an exception executing onHalfClose, we don't expect other lifecycle
+        // commands to succeed. Accordingly, we close the span
+        finishWithError(spanRef.getAndSet(null), e);
+        throw e;
+      }
+    }
+
+    @Override public void request(int numMessages) {
+      try (Scope scope = maybeScopeClientOrInvocationContext(spanRef, invocationContext)) {
+        delegate().request(numMessages);
+      }
+    }
+
+    @Override public void sendMessage(ReqT message) {
+      try (Scope scope = maybeScopeClientOrInvocationContext(spanRef, invocationContext)) {
+        delegate().sendMessage(message);
+        Span span = spanRef.get(); // could be an error
+        SpanCustomizer customizer = span != null ? span.customizer() : NoopSpanCustomizer.INSTANCE;
+        messageProcessor.onMessageSent(message, customizer);
+      }
+    }
+  }
+
+  /** Scopes the client context or the invocation if the client span finished */
+  Scope maybeScopeClientOrInvocationContext(
+    AtomicReference<Span> spanRef,
+    @Nullable TraceContext invocationContext
+  ) {
+    Span span = spanRef.get();
+    TraceContext context = span != null ? span.context() : invocationContext;
+    return currentTraceContext.maybeScope(context);
+  }
+
+  final class TracingClientCallListener<RespT> extends SimpleForwardingClientCallListener<RespT> {
+    @Nullable final TraceContext invocationContext;
+    final AtomicReference<Span> spanRef;
+    final GrpcClientRequest request;
+
+    TracingClientCallListener(
+      Listener<RespT> delegate,
+      @Nullable TraceContext invocationContext,
+      AtomicReference<Span> spanRef,
+      GrpcClientRequest request
+    ) {
+      super(delegate);
+      this.invocationContext = invocationContext;
+      this.spanRef = spanRef;
+      this.request = request;
+    }
+
     @Override public void onReady() {
-      try (Scope scope = currentTraceContext.maybeScope(invocationContext)) {
+      try (Scope scope = maybeScopeClientOrInvocationContext(spanRef, invocationContext)) {
         delegate().onReady();
       }
     }
 
+    // See instrumentation/RATIONALE.md for why the below response callbacks are invocation context
     @Override public void onHeaders(Metadata headers) {
       try (Scope scope = currentTraceContext.maybeScope(invocationContext)) {
         delegate().onHeaders(headers);
@@ -127,38 +199,20 @@ final class TracingClientInterceptor implements ClientInterceptor {
 
     @Override public void onMessage(RespT message) {
       try (Scope scope = currentTraceContext.maybeScope(invocationContext)) {
+        Span span = spanRef.get(); // could be an error
+        SpanCustomizer customizer = span != null ? span.customizer() : NoopSpanCustomizer.INSTANCE;
+        messageProcessor.onMessageReceived(message, customizer);
         delegate().onMessage(message);
       }
     }
 
     @Override public void onClose(Status status, Metadata trailers) {
+      // See /instrumentation/grpc/RATIONALE.md for why we don't catch exceptions from the delegate
+      GrpcClientResponse response = new GrpcClientResponse(request, status, trailers, null);
+      finish(response, spanRef.getAndSet(null));
+
       try (Scope scope = currentTraceContext.maybeScope(invocationContext)) {
         delegate().onClose(status, trailers);
-      }
-    }
-  }
-
-  final class TracingClientCallListener<RespT> extends SimpleForwardingClientCallListener<RespT> {
-    final Span span;
-
-    TracingClientCallListener(Listener<RespT> responseListener, Span span) {
-      super(responseListener);
-      this.span = span;
-    }
-
-    @Override public void onMessage(RespT message) {
-      try (Scope scope = currentTraceContext.maybeScope(span.context())) {
-        parser.onMessageReceived(message, span.customizer());
-        delegate().onMessage(message);
-      }
-    }
-
-    @Override public void onClose(Status status, Metadata trailers) {
-      try (Scope scope = currentTraceContext.maybeScope(span.context())) {
-        super.onClose(status, trailers);
-        parser.onClose(status, trailers, span.customizer());
-      } finally {
-        span.finish();
       }
     }
   }
---
+++ instrumentation/grpc/src/test/java/brave/grpc/GrpcServerResponseTest.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2013-2020 The OpenZipkin Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package brave.grpc;
+
+import io.grpc.Metadata;
+import io.grpc.Metadata.Key;
+import io.grpc.ServerCall;
+import io.grpc.Status;
+import org.junit.Test;
+
+import static java.util.Collections.singletonMap;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+public class GrpcServerResponseTest {
+  Key<String> b3Key = Key.of("b3", Metadata.ASCII_STRING_MARSHALLER);
+  ServerCall call = mock(ServerCall.class);
+  Metadata headers = new Metadata();
+  GrpcServerRequest request =
+    new GrpcServerRequest(singletonMap("b3", b3Key), call, headers);
+  Status status = Status.CANCELLED;
+  Metadata trailers = new Metadata();
+  GrpcServerResponse response = new GrpcServerResponse(request, status, trailers, null);
+
+  @Test public void request() {
+    assertThat(response.request()).isSameAs(request);
+  }
+
+  @Test public void status() {
+    assertThat(response.status()).isSameAs(status);
+  }
+
+  @Test public void unwrap() {
+    assertThat(response.unwrap()).isSameAs(status);
+  }
+
+  @Test public void trailers() {
+    assertThat(response.trailers()).isSameAs(trailers);
+  }
+
+  @Test public void error_null() {
+    assertThat(response.error()).isNull();
+  }
+
+  @Test public void error() {
+    RuntimeException error = new RuntimeException("noodles");
+    GrpcServerResponse response = new GrpcServerResponse(request, status, trailers, error);
+
+    assertThat(response.error()).isSameAs(error);
+  }
+
+  @Test public void error_fromStatus() {
+    RuntimeException error = new RuntimeException("noodles");
+    status = Status.fromThrowable(error);
+    GrpcServerResponse response = new GrpcServerResponse(request, status, trailers, null);
+
+    assertThat(response.error()).isSameAs(error);
+    assertThat(response.errorCode()).isEqualTo("UNKNOWN");
+  }
+
+  @Test public void errorCode_nullWhenOk() {
+    status = Status.OK;
+    GrpcServerResponse response = new GrpcServerResponse(request, status, trailers, null);
+
+    assertThat(response.errorCode()).isNull();
+  }
+
+  @Test public void errorCode() {
+    assertThat(response.errorCode()).isEqualTo("CANCELLED");
+  }
+}
