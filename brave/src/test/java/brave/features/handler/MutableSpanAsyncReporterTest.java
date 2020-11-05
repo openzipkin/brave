a16ff30de21cc88c5fc8bc7179004f53c9d81dbc
Adrian Cole <adriancole@users.noreply.github.com>
2020-11-05 14:31:13 +0800

    Removes root dependency management (#1275)
    This removes root dependency management for same rationale as
    https://github.com/openzipkin/zipkin/pull/3284
    
    Notably, to prevent interference or confusion in downstream projects.
    
    This also forcibly prevents benchmarks from the deploy phase as the
    prior config resulted in orphaned staging repos. See
    https://github.com/openzipkin/zipkin/pull/3286

--- brave/src/test/java/brave/features/handler/MutableSpanAsyncReporterTest.java
+++ brave/src/test/java/brave/features/handler/MutableSpanAsyncReporterTest.java
@@ -20,16 +20,11 @@ import brave.handler.MutableSpanBytesEncoder;
 import brave.handler.SpanHandler;
 import brave.propagation.B3SingleFormat;
 import brave.propagation.TraceContext;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 import org.junit.After;
-import org.junit.Rule;
 import org.junit.Test;
-import zipkin2.codec.BytesEncoder;
-import zipkin2.codec.Encoding;
-import zipkin2.junit.ZipkinRule;
-import zipkin2.reporter.AsyncReporter;
-import zipkin2.reporter.okhttp3.OkHttpSender;
+import zipkin2.codec.SpanBytesDecoder;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -38,79 +33,54 @@ import static org.assertj.core.api.Assertions.assertThat;
  * direct encoding from {@link MutableSpan} into JSON without converting to Zipkin model first.
  */
 public class MutableSpanAsyncReporterTest {
-  @Rule public ZipkinRule zipkin = new ZipkinRule();
+  MutableSpanBytesEncoder mutableSpanBytesEncoder =
+    MutableSpanBytesEncoder.zipkinJsonV2(Tags.ERROR);
 
-  MutableSpanBytesEncoder mutableSpanBytesEncoder = MutableSpanBytesEncoder.zipkinJsonV2(Tags.ERROR);
-  BytesEncoder<MutableSpan> zipkinBytesEncoderAdapter = new BytesEncoder<MutableSpan>() {
-
-    @Override public Encoding encoding() {
-      return Encoding.JSON;
-    }
-
-    @Override public int sizeInBytes(MutableSpan span) {
-      return mutableSpanBytesEncoder.sizeInBytes(span);
-    }
-
-    @Override public byte[] encode(MutableSpan span) {
-      return mutableSpanBytesEncoder.encode(span);
-    }
-
-    @Override public byte[] encodeList(List<MutableSpan> spans) {
-      return mutableSpanBytesEncoder.encodeList(spans);
-    }
-  };
-
-  OkHttpSender sender = OkHttpSender.create(zipkin.httpUrl() + "/api/v2/spans");
-
-  AsyncReporter<MutableSpan> reporter = AsyncReporter.builder(sender)
-      .messageTimeout(0, TimeUnit.MILLISECONDS) // don't spawn a thread
-      .build(zipkinBytesEncoderAdapter);
+  List<byte[]> messages = new ArrayList<>();
 
   SpanHandler spanHandlerAdapter = new SpanHandler() {
     @Override public boolean end(TraceContext context, MutableSpan span, SpanHandler.Cause cause) {
       if (!Boolean.TRUE.equals(context.sampled())) return true;
-      reporter.report(span);
+      messages.add(mutableSpanBytesEncoder.encode(span));
       return true;
     }
   };
 
   Tracing tracing = Tracing.newBuilder()
-      .localServiceName("Aa")
-      .localIp("1.2.3.4")
-      .localPort(80)
-      .addSpanHandler(spanHandlerAdapter)
-      .build();
+    .localServiceName("Aa")
+    .localIp("1.2.3.4")
+    .localPort(80)
+    .addSpanHandler(spanHandlerAdapter)
+    .build();
 
   @After public void close() {
     tracing.close();
-    reporter.close();
-    sender.close();
   }
 
   /** This mainly shows endpoints are taken from Brave, and error is back-filled. */
   @Test public void basicSpan() {
     TraceContext context = B3SingleFormat.parseB3SingleFormat(
-        "50d980fffa300f29-86154a4ba6e91385-1"
+      "50d980fffa300f29-86154a4ba6e91385-1"
     ).context();
 
     tracing.tracer().toSpan(context).name("test")
-        .start(1L)
-        .error(new RuntimeException("this cake is a lie"))
-        .finish(3L);
+      .start(1L)
+      .error(new RuntimeException("this cake is a lie"))
+      .finish(3L);
 
-    reporter.flush();
-
-    assertThat(zipkin.getTraces()).hasSize(1).first().hasToString(
-        "[{\"traceId\":\"50d980fffa300f29\","
-            + "\"id\":\"86154a4ba6e91385\","
-            + "\"name\":\"test\","
-            + "\"timestamp\":1,"
-            + "\"duration\":2,"
-            + "\"localEndpoint\":{"
-            + "\"serviceName\":\"aa\","
-            + "\"ipv4\":\"1.2.3.4\","
-            + "\"port\":80},"
-            + "\"tags\":{\"error\":\"this cake is a lie\"}}]"
-    );
+    assertThat(messages).hasSize(1).first()
+      .extracting(SpanBytesDecoder.JSON_V2::decodeOne)
+      .hasToString(
+        "{\"traceId\":\"50d980fffa300f29\","
+          + "\"id\":\"86154a4ba6e91385\","
+          + "\"name\":\"test\","
+          + "\"timestamp\":1,"
+          + "\"duration\":2,"
+          + "\"localEndpoint\":{"
+          + "\"serviceName\":\"aa\","
+          + "\"ipv4\":\"1.2.3.4\","
+          + "\"port\":80},"
+          + "\"tags\":{\"error\":\"this cake is a lie\"}}"
+      );
   }
 }
