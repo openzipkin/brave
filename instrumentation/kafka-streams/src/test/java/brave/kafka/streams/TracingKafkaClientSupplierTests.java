5e0ee5f25754bfa01f8da3cc13b44528b0f2f2bf
Tim Ysewyn <Tim.Ysewyn@me.com>
2021-12-17 19:03:05 +0100

    Added tests

---
+++ instrumentation/kafka-streams/src/test/java/brave/kafka/streams/TracingKafkaClientSupplierTests.java
@@ -0,0 +1,56 @@
+package brave.kafka.streams;
+
+import java.util.Collections;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.streams.KafkaClientSupplier;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class TracingKafkaClientSupplierTests {
+
+  final Map<String, Object> props = Collections.singletonMap("bootstrap.servers","localhost:9092");
+
+  @Test
+  public void shouldReturnNewAdminClient() {
+    TracingKafkaClientSupplier supplier = new TracingKafkaClientSupplier(null);
+    assertThat(supplier.getAdminClient(props)).isNotNull();
+    assertThat(supplier.getAdmin(props)).isNotNull();
+  }
+
+  @Test
+  void shouldThrowException() {
+    assertThrows(UnsupportedOperationException.class, () -> {
+      FakeKafkaClientSupplier fake = new FakeKafkaClientSupplier();
+      fake.getAdminClient(props);
+    });
+  }
+
+  private static class FakeKafkaClientSupplier implements KafkaClientSupplier {
+
+    @Override
+    public Producer<byte[], byte[]> getProducer(Map<String, Object> map) {
+      return null;
+    }
+
+    @Override
+    public Consumer<byte[], byte[]> getConsumer(Map<String, Object> map) {
+      return null;
+    }
+
+    @Override
+    public Consumer<byte[], byte[]> getRestoreConsumer(Map<String, Object> map) {
+      return null;
+    }
+
+    @Override
+    public Consumer<byte[], byte[]> getGlobalConsumer(Map<String, Object> map) {
+      return null;
+    }
+  }
+
+}
