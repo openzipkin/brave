d4cfd8e554b0835783de897800ef9b863b719003
jainigo <jainigo@profile.es>
2021-12-22 21:26:04 +0100

    explicit imports

--- instrumentation/kafka-clients/src/main/java/brave/kafka/clients/TracingConsumer.java
+++ instrumentation/kafka-clients/src/main/java/brave/kafka/clients/TracingConsumer.java
@@ -23,7 +23,11 @@ import brave.sampler.SamplerFunction;
 
 import java.lang.reflect.Method;
 import java.time.Duration;
-import java.util.*;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.Collection;
+import java.util.LinkedHashMap;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
 import org.apache.kafka.clients.consumer.Consumer;
