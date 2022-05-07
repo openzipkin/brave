3addc7fb4e7c3d5f8e4d58cef0420d640f122509
zipkinci <zipkinci+zipkin-dev@googlegroups.com>
2022-05-07 09:36:35 +0000

    Automatically updated javadocs for 5.13.9

---
+++ 5.13.9/brave-instrumentation-spring-web/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"brave.spring.web","l":"TracingAsyncClientHttpRequestInterceptor"},{"p":"brave.spring.web","l":"TracingClientHttpRequestInterceptor"}]
