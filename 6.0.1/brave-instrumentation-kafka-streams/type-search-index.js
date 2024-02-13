c2afd166c0330a7b18b8f67d48a93943386fdb74
zipkinci <zipkinci+zipkin-dev@googlegroups.com>
2024-02-13 10:54:33 +0000

    Automatically updated javadocs for 6.0.1

---
+++ 6.0.1/brave-instrumentation-kafka-streams/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"brave.kafka.streams","l":"KafkaStreamsTracing.Builder"},{"p":"brave.kafka.streams","l":"KafkaStreamsTracing"}]
